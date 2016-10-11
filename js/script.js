3e2eb23df797792b73a4885010b669c84f232859
Oliver Schwendener <oliver.schwendener@uni.li>
2016-10-11 12:21:34 +0200

    added source files

--- js/script.js
+++ js/script.js
@@ -1,76 +1,45 @@
-// Mixing jQuery and Node.js code in the same file? Yes please!
-
 $(function(){
-
-    // Display some statistics about this computer, using node's os module.
-
+    var fs = require('fs');
     var os = require('os');
-    var prettyBytes = require('pretty-bytes');
+    var path = require('path');
 
-    $('.stats').append('Number of cpu cores: <span>' + os.cpus().length + '</span>');
-    $('.stats').append('Free memory: <span>' + prettyBytes(os.freemem())+ '</span>');
+    var exec = require('child_process').exec;
 
-    // Electron's UI library. We will need it for later.
-
-    var shell = require('shell');
-
-
-    // Fetch the recent posts on Tutorialzine.
-
-    var ul = $('.flipster ul');
-
-    // The same-origin security policy doesn't apply to electron, so we can
-    // send ajax request to other sites. Let's fetch Tutorialzine's rss feed:
-
-    $.get('http://feeds.feedburner.com/Tutorialzine', function(response){
-
-        var rss = $(response);
-
-        // Find all articles in the RSS feed:
-
-        rss.find('item').each(function(){
-            var item = $(this);
-            
-            var content = item.find('encoded').html().split('</a></div>')[0]+'</a></div>';
-            var urlRegex = /(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?/g;
-
-            // Fetch the first image of the article.
-            var imageSource = content.match(urlRegex)[1];
-
-
-            // Create a li item for every article, and append it to the unordered list.
-
-            var li = $('<li><img /><a target="_blank"></a></li>');
-
-            li.find('a')
-                .attr('href', item.find('link').text())
-                .text(item.find("title").text());
-
-            li.find('img').attr('src', imageSource);
-
-            li.appendTo(ul);
-
+    var walk = function(dir) {
+        var results = [];
+        var list = fs.readdirSync(dir)
+        list.forEach(function(file) {
+            file = dir + '/' + file;
+            var stat = fs.statSync(file);
+            if (stat && stat.isDirectory()) results = results.concat(walk(file));
+            else
+                if(path.extname(file) === '.lnk' || path.extname(file) === '.exe') 
+                    results.push(file);
         });
+        return results;
+    };
 
-        // Initialize the flipster plugin.
+    var homeDir = os.homedir();
+    var userStartMenuFolderPath = homeDir + '\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu';
+    var commonStartMenuFolderPath = 'C:\\ProgramData\\Microsoft\\Windows\\Start Menu';
 
-        $('.flipster').flipster({
-            style: 'carousel'
-        });
+    var shortCutFiles = walk(userStartMenuFolderPath);
+    var commonShortCutFiles = walk(commonStartMenuFolderPath);
 
-        // When an article is clicked, open the page in the system default browser.
-        // Otherwise it would open it in the electron window which is not what we want.
+    console.log(shortCutFiles);
+    console.log(commonShortCutFiles);
 
-        $('.flipster').on('click', 'a', function (e) {
-
-            e.preventDefault();
-            
-            // Open URL with default browser.
-
-            shell.openExternal(e.target.href);
-
-        });
-
+    // When user hits enter on keyboard
+    $('input').keyup(function(e) {
+        if(e.keyCode === 13){
+            var path = '';
+            StartProcess('C:\\Temp\\test2.lnk');
+        }
     });
 
+    function StartProcess(pathToLnk){
+        var cmd = exec('start ' + pathToLnk, function(error, stdout, stderr){
+            if(error) throw error;
+        });
+    }
 });
--- main.js
+++ main.js
@@ -22,7 +22,7 @@ app.on('ready', function() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
-    height: 200,
+    height: 600,
     frame:false
   });
 
