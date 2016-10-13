8cffc79e2e941b78f2dce6e1b99fcdfc5df2e6fd
Oliver Schwendener <oliver.schwendener@uni.li>
2016-10-13 15:11:42 +0200

    code formatting

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var electron = require('electron');  // Module to control application life.
+var electron = require('electron');
 
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
@@ -9,51 +9,50 @@ var mainWindow = null;
 var optionsWindow = null;
 
 var mainWindowOptions = {
-    width: 600,
-    height: 150,
-    frame:false,
-    //resizable: true,
-    //devTools: true,
-    transparent: true,
-    skipTaskbar: true,
-    show: false,
-    center: true
-  };
+  width: 600,
+  height: 150,
+  frame: false,
+  //resizable: true,
+  //devTools: true,
+  transparent: true,
+  skipTaskbar: true,
+  show: false,
+  center: true
+};
 
 var mainWindowHtml = 'file://' + __dirname + '/main.html';
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function(){
-  if (process.platform != 'darwin'){
+app.on('window-all-closed', function () {
+  if (process.platform != 'darwin') {
     app.quit();
   }
 });
 
-app.on('ready', function(){
+app.on('ready', function () {
   mainWindow = new BrowserWindow(mainWindowOptions);
 
   mainWindow.loadURL(mainWindowHtml);
 
-  mainWindow.on('closed', function(){
+  mainWindow.on('closed', function () {
     mainWindow = null;
   });
 
-  mainWindow.on('blur', function(){
+  mainWindow.on('blur', function () {
     mainWindow.hide();
   });
 
-  globalShortcut.register('alt+space', function(){
-    if(mainWindow.isVisible())
+  globalShortcut.register('alt+space', function () {
+    if (mainWindow.isVisible())
       mainWindow.hide();
     else
       mainWindow.show();
   });
 
-  ipcMain.on('hide-main-window', function(){
+  ipcMain.on('hide-main-window', function () {
     mainWindow.hide();
   });
 
-  ipcMain.on('close-main-window', function(){
+  ipcMain.on('close-main-window', function () {
     app.quit();
   })
 });
