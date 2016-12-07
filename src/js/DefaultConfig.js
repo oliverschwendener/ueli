094f424af4ca136c510e16fdcfb882d16adb6432
Oliver Schwendener <oliver.schwendener@uni.li>
2016-12-07 15:53:18 +0100

    Added DuckDuckGo to default settings

--- src/js/DefaultConfig.js
+++ src/js/DefaultConfig.js
@@ -1,8 +1,8 @@
 import os from 'os';
 
 export default class DefaultConfig {
-    GetConfig() {
-        return {
+    constructor() {
+        this.config = {
             theme: 'win10',
             maxResultItems: 10,
             folders: [
@@ -15,20 +15,26 @@ export default class DefaultConfig {
                     "prefix": "g",
                     "url": "google.com/search?q=",
                     "fontAwesomeIconClass": "fa-google"
-                },
-                {
+                }, {
                     "name": "Wikipedia",
                     "prefix": "w",
                     "url": "wikipedia.org/w/?search=",
                     "fontAwesomeIconClass": "fa-wikipedia-w"
-                },
-                {
+                }, {
                     "name": "YouTube",
                     "prefix": "yt",
                     "url": "youtube.com/results?search_query=",
                     "fontAwesomeIconClass": "fa-youtube"
+                }, {
+                    "name": "DuckDuckGo",
+                    "prefix": "d",
+                    "url": "duckduckgo.com/?q="
                 }
             ]
         }
+    }
+
+    GetConfig() {
+        return this.config;
     }
 }
