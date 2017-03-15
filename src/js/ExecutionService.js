1e41dc33f9f33c0b8611ee09ac878579f2d926a7
Oliver Schwendener <oliver.schwendener@uni.li>
2017-03-15 07:18:41 +0100

    removed unused code

--- src/js/ExecutionService.js
+++ src/js/ExecutionService.js
@@ -2,24 +2,9 @@ import { exec } from 'child_process'
 import path from 'path'
 import fs from 'fs'
 
-import ProgramExecutor from './Executors/ProgramExecutor'
-import WebUrlExecutor from './Executors/WebUrlExecutor'
 import FilePreviewManager from './FilePreview/FilePreviewManager'
 
 export default class ExecutionService {
-    constructor() {
-        this.executors = [
-            new ProgramExecutor(),
-            new WebUrlExecutor()
-        ]
-    }
-
-    execute(execArgs) {
-        for (let executor of this.executors)
-            if (executor.isValid(execArgs))
-                executor.execute(execArgs)
-    }
-
     openFileLocation(filePath) {
         filePath = path.win32.normalize(filePath)
         let command = `start explorer.exe /select,"${filePath}"`
