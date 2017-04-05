139a9f2e80cf0bff3747d933c8e29d23d0a3ade1
Oliver Schwendener <oliver.schwendener@uni.li>
2017-04-05 15:24:48 +0200

    show mime type in FileInfoPreview

--- src/js/FilePreview/FileInfoPreview.js
+++ src/js/FilePreview/FileInfoPreview.js
@@ -1,15 +1,18 @@
 import fs from 'fs'
 import path from 'path'
+import mime from 'mime'
 
 export default class FileInfoPreview {
     getFilePreview(filePath) {
         let stats = fs.statSync(filePath)
         let fileName = path.basename(filePath)
+        let mimeType = mime.lookup(filePath)
 
         return `<div class="file-info-preview">
                     <h1>${fileName}</h1>
                     <p>File path: ${filePath}</p>
                     <p>File size: ${fileSizeToString(stats.size)}</p>
+                    <p>Mime type: ${mimeType}</p>
                     <p>Last access: ${stats.atime.toLocaleString('de-CH')}</p>
                     <p>Last modified: ${stats.mtime.toLocaleString('de-CH')}</p>
                     <p>Last changed: ${stats.ctime.toLocaleString('de-CH')}</p>
