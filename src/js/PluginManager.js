2afa88fc12923ac0a8de5b92b4bc9288f57da1bb
Oliver Schwendener <oliver.schwendener@hotmail.com>
2018-01-16 21:10:41 +0100

    Show default icon when icon is not set

--- src/js/PluginManager.js
+++ src/js/PluginManager.js
@@ -17,6 +17,12 @@ let stringHelpers = new StringHelpers()
 export default class PluginManager {
     constructor() {
         this.plugins = []
+        this.defaultIcon = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" version="1.1">
+                                <g id="surface1">
+                                    <path d="M 19 3 C 13.488281 3 9 7.488281 9 13 C 9 15.394531 9.839844 17.589844 11.25 19.3125 L 3.28125 27.28125 L 4.71875 28.71875 L 12.6875 20.75 C 14.410156 22.160156 16.605469 23 19 23 C 24.511719 23 29 18.511719 29 13 C 29 7.488281 24.511719 3 19 3 Z M 19 5 C 23.429688 5 27 8.570313 27 13 C 27 17.429688 23.429688 21 19 21 C 14.570313 21 11 17.429688 11 13 C 11 8.570313 14.570313 5 19 5 Z "></path>
+                                </g>
+                            </svg>`
+
         this.setPluginsFromConfig()
         this.favorites = new FavoritesManager().getFavorites()
         this.reloadFavorites(30)
@@ -49,6 +55,9 @@ export default class PluginManager {
         for (let item of unsortedResult) {
             item.weight = stringHelpers.getWeight(item.name, userInput)
             item.isActive = false
+
+            if (stringHelpers.stringIsEmptyOrWhitespaces(item.icon))
+                item.icon = this.defaultIcon
         }
 
         let resultWithFavorites = this.addFavoritesToSearchResult(unsortedResult)
