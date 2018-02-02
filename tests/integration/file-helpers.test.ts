2c060348d00407ff16ffd5fd6a64a172ab54a717
Oliver Schwendener <oliver.schwendener@hotmail.com>
2018-02-02 14:52:41 +0100

    Creating test folders and files in file helper integration test

--- tests/integration/file-helpers.test.ts
+++ tests/integration/file-helpers.test.ts
@@ -1,29 +1,64 @@
 import * as path from "path";
 import * as os from "os";
+import * as fs from "fs";
 import { expect } from "chai";
 import { FileHelpers } from "./../../src/ts/helpers/file-helpers";
+
+const testFolderPaths = [
+    path.join(os.homedir(), "test-folder"),
+    path.join(os.homedir(), "test-folder-2")
+];
+
+const testFiles = [
+    "test-file",
+    "test-file-2",
+    "test-file-3"
+];
+
+function createTestFolders(folderPaths: string[]): void {
+    for (let folderPath of folderPaths) {
+        fs.mkdirSync(folderPath);
+
+        for (let file of testFiles) {
+            fs.writeFileSync(path.join(folderPath, file), "This is a test file", 'utf-8');
+        }
+    }
+}
+
+function deleteTestFolders(folderPaths: string[]): void {
+    for (let folderPath of folderPaths) {
+        for (let file of testFiles) {
+            fs.unlinkSync(path.join(folderPath, file));
+        }
+
+        fs.rmdirSync(folderPath);
+    }
+}
 
 describe("File Helpers", () => {
     describe("getFilesFromFolderRecursively", () => {
         it("should return some files", () => {
-            let testFolderPath = path.join(os.homedir(), "Downloads");
+            let testFolder = testFolderPaths[0];
+            createTestFolders([testFolder]);
 
-            let actual = FileHelpers.getFilesFromFolderRecursively(testFolderPath);
+            let actual = FileHelpers.getFilesFromFolderRecursively(testFolder);
 
             expect(actual.length).to.be.greaterThan(0);
+
+            deleteTestFolders([testFolder])
         });
     });
 
     describe("getFilesFromFoldersRecursivley", () => {
         it("should return some files", () => {
-            let testFolderPaths = [
-                path.join(os.homedir(), "Downloads"),
-                path.join(os.homedir(), "Desktop")
-            ];
+            let testFolders = testFolderPaths;
+            createTestFolders(testFolders);
 
-            let actual = FileHelpers.getFilesFromFoldersRecursively(testFolderPaths);
+            let actual = FileHelpers.getFilesFromFoldersRecursively(testFolders);
 
             expect(actual.length).to.be.greaterThan(0);
+
+            deleteTestFolders(testFolders);
         });
     });
 });
