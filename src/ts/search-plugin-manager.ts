3eff111ee693ea01a02136a8f7da7352041a829a
Oliver Schwendener <oliver.schwendener@hotmail.com>
2018-02-15 19:24:04 +0100

    Renamed files

--- src/ts/search-plugin-manager.ts
+++ src/ts/search-plugin-manager.ts
@@ -3,9 +3,9 @@ import { ProgramsPlugin } from "./search-plugins/programs-plugin";
 import { HomeFolderSearchPlugin } from "./search-plugins/home-folder-plugin";
 import { ElectronizrCommandsSearchPlugin } from "./search-plugins/electronizr-commands-plugin";
 import { Injector, OperatingSystem } from "./injector";
-import { WindowsSettingsSearchPlugin } from "./search-plugins/windows-settings-plugin";
+import { Windows10SettingsSearchPlugin } from "./search-plugins/windows-10-settings-plugin";
 import { Config } from "./config";
-import { Windows10AppsSearchPlugin } from "./search-plugins/windows-apps-plugin";
+import { Windows10AppsSearchPlugin } from "./search-plugins/windows-10-apps-plugin";
 
 export class SearchPluginManager {
     private os = Injector.getCurrentOperatingSystem();
@@ -19,7 +19,7 @@ export class SearchPluginManager {
         ];
 
         if (this.os === OperatingSystem.Windows && Config.searchOperatinSystemSettings) {
-            this.plugins.push(new WindowsSettingsSearchPlugin());
+            this.plugins.push(new Windows10SettingsSearchPlugin());
         }
 
         if (this.os === OperatingSystem.Windows && Config.searchWindows10Apps) {
