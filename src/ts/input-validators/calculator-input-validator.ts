cca6cca867e9832a8c89243bfb910474a8243500
Nguyen Xuan Khanh <xuankhanh963@gmail.com>
2018-05-01 00:44:19 +0700

    Added more valid check for evaluated result

--- src/ts/input-validators/calculator-input-validator.ts
+++ src/ts/input-validators/calculator-input-validator.ts
@@ -5,11 +5,25 @@ import * as math from "mathjs";
 export class CalculatorInputValidator implements InputValidator {
     public isValidForSearchResults(userInput: string): boolean {
         try {
-            // Mathjs throws an error when input cannot be evaluate
-            math.eval(userInput);
+            // Mathjs throws an error when input cannot be evaluated
+            var result = math.eval(userInput);
         } catch (e) {
             return false;
         }
+        return !isNaN(result) || this.isValidObject(result) || false;
+    }
+
+    private isValidObject (input: any): boolean {
+        if (typeof(input) !== 'object') {
+            return false;
+        }
+
+        var mathType = math.typeof(input);
+
+        if (mathType === 'Unit' && input.value === null) {
+            return false;
+        }
+
         return true;
     }
 }
