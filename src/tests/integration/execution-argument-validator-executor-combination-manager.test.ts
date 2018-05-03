00e9b0e7a330c2454425534fba05325698198e5a
Oliver Schwendener <oliver.schwendener@hotmail.com>
2018-05-03 19:32:12 +0200

    Added check for executionargument validator executor combination manager

---
+++ src/tests/integration/execution-argument-validator-executor-combination-manager.test.ts
@@ -0,0 +1,34 @@
+import { ExecutionArgumentValidatorExecutorCombinationManager } from "../../ts/execution-argument-validator-executor-combination-manager";
+import { platform } from "os";
+import { WindowsSettingsExecutor } from "../../ts/executors/windows-settings-executor";
+import { WindowsSettingsExecutionArgumentValidator } from "../../ts/execution-argument-validators/windows-settings-execution-argument-validator";
+import { MacOsSettingsExecutionArgumentValidator } from "../../ts/execution-argument-validators/mac-os-execution-argument-validator";
+import { MacOsSettingsExecutor } from "../../ts/executors/mac-os-settings-executor";
+import { OperatingSystemHelpers } from "../../ts/helpers/operating-system-helpers";
+import { OperatingSystem } from "../../ts/operating-system";
+
+describe(ExecutionArgumentValidatorExecutorCombinationManager.name, (): void => {
+    it("should return the correct execution argument valiator executor combinations", (): void => {
+        let counter = 0;
+
+        const actual = ExecutionArgumentValidatorExecutorCombinationManager.ValidatorExecutorCombinations;
+
+        const currentOperatingSystem = OperatingSystemHelpers.getOperatingSystemFromString(platform());
+
+        for (const combination of actual) {
+            if (currentOperatingSystem === OperatingSystem.Windows) {
+                if (combination.validator instanceof WindowsSettingsExecutionArgumentValidator
+                    && combination.executor instanceof WindowsSettingsExecutor) {
+                    counter++;
+                }
+            } else if (currentOperatingSystem === OperatingSystem.macOS) {
+                if (combination.validator instanceof MacOsSettingsExecutionArgumentValidator
+                    && combination.executor instanceof MacOsSettingsExecutor) {
+                    counter++;
+                }
+            }
+        }
+
+        expect(counter).toBe(1);
+    });
+});
