bbafe24d25edfb0b629dc8d0a5361bb6747d0309
Oliver Schwendener <oliver.schwendener@hotmail.com>
2018-05-12 10:50:12 +0200

    Added lock system command for windows

--- src/ts/search-plugins/windows-10-settings-plugin.ts
+++ src/ts/search-plugins/windows-10-settings-plugin.ts
@@ -912,7 +912,17 @@ export class Windows10SettingsSearchPlugin implements SearchPlugin {
                             </g>
                         </svg>`,
                 name: "Sign out",
-                tags: ["out", "off", "sign", "user"],
+                tags: ["out", "off", "sign", "user", "log"],
+            },
+            {
+                executionArgument: `${WindowsSettingsHelpers.windowsSettingsPrefix}rundll32 user32.dll,LockWorkStation`,
+                icon: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" version="1.1">
+                            <g id="surface1">
+                                <path style=" " d="M 16 3 C 12.15625 3 9 6.15625 9 10 L 9 13 L 6 13 L 6 29 L 26 29 L 26 13 L 23 13 L 23 10 C 23 6.15625 19.84375 3 16 3 Z M 16 5 C 18.753906 5 21 7.246094 21 10 L 21 13 L 11 13 L 11 10 C 11 7.246094 13.246094 5 16 5 Z M 8 15 L 24 15 L 24 27 L 8 27 Z "></path>
+                            </g>
+                        </svg>`,
+                name: "Lock computer",
+                tags: [],
             },
             {
                 executionArgument: `${WindowsSettingsHelpers.windowsSettingsPrefix}winver`,
