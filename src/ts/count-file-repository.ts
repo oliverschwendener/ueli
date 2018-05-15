680efa841c378eaf273727e532be92935b8a3574
Oliver Schwendener <oliver.schwendener@hotmail.com>
2018-05-15 15:43:17 +0200

    Added missing methods

--- src/ts/count-file-repository.ts
+++ src/ts/count-file-repository.ts
@@ -5,4 +5,12 @@ export class CountFileRepository implements CountRepository {
     public getCount(): Count {
         throw new Error("not implemented");
     }
+
+    public getScore(key: string): number {
+        throw new Error("Method not implemented.");
+    }
+
+    public updateCount(count: Count): void {
+        throw new Error("Method not implemented.");
+    }
 }
