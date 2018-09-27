118a04b78dffd06ce083593c4ade5cae5ddec8df
Oliver Schwendener <oliver.schwendener@hotmail.com>
2018-09-27 07:25:50 +0200

    Replace class with interface

--- src/ts/input-validator-searcher-combination.ts
+++ src/ts/input-validator-searcher-combination.ts
@@ -1,7 +1,7 @@
 import { InputValidator } from "./input-validators/input-validator";
 import { Searcher } from "./searcher/searcher";
 
-export class InputValidatorSearcherCombination {
-    public searcher: Searcher;
-    public validator: InputValidator;
+export interface InputValidatorSearcherCombination {
+    searcher: Searcher;
+    validator: InputValidator;
 }
