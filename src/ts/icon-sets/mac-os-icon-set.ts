8c52b89d1c27e5989f325b52e0562c118848db8d
Oliver Schwendener <oliver.schwendener@hotmail.com>
2018-09-27 07:03:22 +0200

    Removed inline height and width from svg

--- src/ts/icon-sets/mac-os-icon-set.ts
+++ src/ts/icon-sets/mac-os-icon-set.ts
@@ -41,7 +41,7 @@ export const MacOsIconSet: IconSet = {
                         </svg>`,
 
     operatingSystemSettingsIcon:  `
-    <svg width="506px" height="477px" viewBox="0 0 506 477" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <svg viewBox="0 0 506 477" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
         <!-- Generator: Sketch 51.3 (57544) - http://www.bohemiancoding.com/sketch -->
         <desc>Created with Sketch.</desc>
         <defs>
