0dc740aaf250a640fdebb17cfd328082b5b0ee18
Oliver Schwendener <oliver.schwendener@hotmail.com>
2018-11-22 14:40:27 +0100

    Normalizing file path when getting native file icon

--- src/ts/production-icon-service.ts
+++ src/ts/production-icon-service.ts
@@ -2,12 +2,13 @@ import { NativeImage } from "electron";
 import { SearchResultItem } from "./search-result-item";
 import { IconSet } from "./icon-sets/icon-set";
 import { app } from "electron";
+import { normalize } from "path";
 
 export class ProductionIconService {
     public getProgramIcon(iconSet: IconSet, searchResultItem: SearchResultItem ): Promise<SearchResultItem> {
         return new Promise((resolve, reject) => {
             if (searchResultItem.icon === iconSet.appIcon || searchResultItem.icon === iconSet.fileIcon) {
-                app.getFileIcon(searchResultItem.executionArgument, { size: "normal" }, (error: Error, icon: NativeImage) => {
+                app.getFileIcon(normalize(searchResultItem.executionArgument), { size: "normal" }, (error: Error, icon: NativeImage) => {
                     if (error) {
                         resolve(searchResultItem);
                     } else {
