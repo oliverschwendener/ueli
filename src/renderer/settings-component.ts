b0e5757484baff9d5f7118a5dd27e8b13d1d1bcf
Oliver Schwendener <oliver.schwendener@hotmail.com>
2019-02-05 23:00:53 +0100

    Use bulma container class for settings

--- src/renderer/settings-component.ts
+++ src/renderer/settings-component.ts
@@ -78,7 +78,7 @@ export const settingsComponent = Vue.extend({
         });
     },
     template: `
-        <div class="settings">
+        <div class="settings container">
             <settings-loading-overlay></settings-loading-overlay>
             <div class="settings__sidebar menu">
                 <ul class="menu-list">
