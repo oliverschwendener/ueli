bc30d7e1f674d3e52cec6113e9039fcd1ef8741c
Oliver Schwendener <oliver.schwendener@hotmail.com>
2019-02-10 15:27:21 +0100

    Linting

--- src/main/plugins/everything/everthing-search-plugin.ts
+++ src/main/plugins/everything/everthing-search-plugin.ts
@@ -11,8 +11,8 @@ import { Icon } from "../../../common/icon/icon";
 export class EverythingSearchPlugin implements ExecutionPlugin {
     public pluginType: PluginType = PluginType.EverythingSearchPlugin;
     private config: UserConfigOptions;
-    private filePathExecutor: (filePath: string) => Promise<void>;
-    private defaultIcon: Icon = {
+    private readonly filePathExecutor: (filePath: string) => Promise<void>;
+    private readonly defaultIcon: Icon = {
         parameter: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" version="1.1">
         <g id="surface1">
         <path style=" " d="M 6 3 L 6 29 L 26 29 L 26 9.59375 L 25.71875 9.28125 L 19.71875 3.28125 L 19.40625 3 Z M 8 5 L 18 5 L 18 11 L 24 11 L 24 27 L 8 27 Z M 20 6.4375 L 22.5625 9 L 20 9 Z "></path>
