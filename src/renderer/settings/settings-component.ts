1ad68a7b285a27aff3f1d0ca8488db64b276fca2
Oliver Schwendener <oliver.schwendener@hotmail.com>
2019-05-11 00:17:08 +0200

    Use SVG image in settings header

--- src/renderer/settings/settings-component.ts
+++ src/renderer/settings/settings-component.ts
@@ -79,7 +79,7 @@ export const settingsComponent = Vue.extend({
             <settings-loading-overlay></settings-loading-overlay>
             <div class="settings__sidebar menu">
                 <div class="settings__sidebar-header-container">
-                    <img class="settings__sidebar-header-image" src="./assets/ueli-black-on-transparent-logo.png">
+                    <img class="settings__sidebar-header-image" src="./assets/ueli.svg">
                     <span class="settings__sidebar-header-title">{{ translations.settings }}</span>
                 </div>
                 <div class="menu-label">
