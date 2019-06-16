d0d84bdf4b2e84a87a68e8eed934dc8e0f8e29bf
Oliver Schwendener <oliver.schwendener@hotmail.com>
2019-06-16 09:54:42 +0200

    Implemented windows operating system setting executor

--- src/main/executors/operating-system-setting-executor.ts
+++ src/main/executors/operating-system-setting-executor.ts
@@ -1,9 +1,10 @@
 import { executeFilePathMacOs } from "./file-path-executor";
+import { executeCommand } from "./command-executor";
 
 export function executeMacOSOperatingSystemSetting(executionArgument: string): Promise<void> {
     return executeFilePathMacOs(executionArgument, false);
 }
 
 export function executeWindowsOperatingSystemSetting(executionArgument: string): Promise<void> {
-    throw new Error("not implemented");
+    return executeCommand(`start ${executionArgument}`);
 }
