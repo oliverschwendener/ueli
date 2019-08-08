ee2cae39bddef273042df161e4f436e7a5a82a72
Oliver Schwendener <oliver.schwendener@hotmail.com>
2019-08-08 13:22:48 +0200

    Added test for email-helpers.ts

--- src/common/helpers/email-helpers.ts
+++ src/common/helpers/email-helpers.ts
@@ -1,3 +1,7 @@
-export function isValidEmailAddress(emailAddress: string): boolean {
-    return new RegExp(new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)).test(emailAddress);
+export function isValidEmailAddress(emailAddress: string | undefined | null): boolean {
+    if (!emailAddress) {
+        return false;
+    }
+
+    return new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(emailAddress);
 }
