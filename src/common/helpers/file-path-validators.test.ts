793063e60991ce661354a36cc9798be613b6c955
Oliver Schwendener <oliver.schwendener@hotmail.com>
2019-08-08 13:49:05 +0200

    Improved file path validators

--- src/common/helpers/file-path-validators.test.ts
+++ src/common/helpers/file-path-validators.test.ts
@@ -27,36 +27,34 @@ const invalidMacOsFilePaths = [
     "-/Gugus",
 ];
 
-describe("File path validators", () => {
-    describe(isValidWindowsFilePath.name, () => {
-        it("should return true if given string is a valid file path", () => {
-            validWindowsFilePaths.forEach((validWindowsFilePath) => {
-                const actual = isValidWindowsFilePath(validWindowsFilePath);
-                expect(actual).toBe(true);
-            });
-        });
-
-        it("should return false if given string is an invalid file path", () => {
-            invalidWindowsFilePaths.forEach((invalidWindowsFilePath) => {
-                const actual = isValidWindowsFilePath(invalidWindowsFilePath);
-                expect(actual).toBe(false);
-            });
+describe(isValidWindowsFilePath.name, () => {
+    it("should return true if given string is a valid file path", () => {
+        validWindowsFilePaths.forEach((validWindowsFilePath) => {
+            const actual = isValidWindowsFilePath(validWindowsFilePath);
+            expect(actual).toBe(true);
         });
     });
 
-    describe(isValidMacOsFilePath.name, () => {
-        it("should return true if given string is a valid file path", () => {
-            validMacOsFilePaths.forEach((validMacOsFilePath) => {
-                const actual = isValidMacOsFilePath(validMacOsFilePath);
-                expect(actual).toBe(true);
-            });
+    it("should return false if given string is an invalid file path", () => {
+        invalidWindowsFilePaths.forEach((invalidWindowsFilePath) => {
+            const actual = isValidWindowsFilePath(invalidWindowsFilePath);
+            expect(actual).toBe(false);
         });
+    });
+});
 
-        it("should return false if given string is an invalid file path", () => {
-            invalidMacOsFilePaths.forEach((invalidMacOsFilePath) => {
-                const actual = isValidMacOsFilePath(invalidMacOsFilePath);
-                expect(actual).toBe(false);
-            });
+describe(isValidMacOsFilePath.name, () => {
+    it("should return true if given string is a valid file path", () => {
+        validMacOsFilePaths.forEach((validMacOsFilePath) => {
+            const actual = isValidMacOsFilePath(validMacOsFilePath);
+            expect(actual).toBe(true);
+        });
+    });
+
+    it("should return false if given string is an invalid file path", () => {
+        invalidMacOsFilePaths.forEach((invalidMacOsFilePath) => {
+            const actual = isValidMacOsFilePath(invalidMacOsFilePath);
+            expect(actual).toBe(false);
         });
     });
 });
