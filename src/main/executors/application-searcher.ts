cda81ccd4d0b515e56f0948fda528f0df7e1294a
Oliver Schwendener <oliver.schwendener@hotmail.com>
2019-09-16 10:02:33 +0200

    Respect application folders in mac app search

--- src/main/executors/application-searcher.ts
+++ src/main/executors/application-searcher.ts
@@ -1,7 +1,6 @@
-import { executeCommandWithOutput } from "./command-executor";
-
 import { normalize } from "path";
 import { ApplicationSearchOptions } from "../../common/config/application-search-options";
+import { executeCommandWithOutput } from "./command-executor";
 
 export function searchWindowsApplications(applicationSearchOptions: ApplicationSearchOptions): Promise<string[]> {
     return new Promise((resolve, reject) => {
@@ -29,15 +28,23 @@ export function searchWindowsApplications(applicationSearchOptions: ApplicationS
 
 export function searchMacApplications(applicationSearchOptions: ApplicationSearchOptions): Promise<string[]> {
     return new Promise((resolve, reject) => {
-        executeCommandWithOutput(`mdfind "kind:apps"`)
-            .then((data) => {
-                const filePaths = data
-                    .split("\n")
-                    .map((f) => normalize(f).trim())
-                    .filter((f) => f.length > 2);
+        if (applicationSearchOptions.applicationFolders.length === 0) {
+            resolve([]);
+        } else {
+            const folderRegex = applicationSearchOptions.applicationFolders.map((applicationFolder) => {
+                return `^${applicationFolder.replace("\\", "\\\\").replace("/", "\\/")}`;
+            }).join("|");
 
-                resolve(filePaths);
-            })
-            .catch((err) => reject(err));
+            executeCommandWithOutput(`mdfind "kind:apps" | egrep "${folderRegex}"`)
+                .then((data) => {
+                    const filePaths = data
+                        .split("\n")
+                        .map((f) => normalize(f).trim())
+                        .filter((f) => f.length > 2);
+
+                    resolve(filePaths);
+                })
+                .catch((err) => reject(err));
+        }
     });
 }
