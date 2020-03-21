64e36e613a33484aba647dcffe06c2c3ffa16932
Oliver Schwendener <oliver.schwendener@hotmail.com>
2020-03-21 19:11:56 +0100

    Handle error when reading Info.plist fails (Fixes #261)

--- src/main/plugins/application-search-plugin/mac-os-app-icon-generator.ts
+++ src/main/plugins/application-search-plugin/mac-os-app-icon-generator.ts
@@ -7,6 +7,10 @@ import { executeCommand } from "../../executors/command-executor";
 
 const defaultIcnsFilePath = "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/GenericApplicationIcon.icns";
 
+interface InfoPlist {
+    CFBundleIconFile?: string;
+}
+
 export function generateMacAppIcons(applicationFilePaths: string[]): Promise<void> {
     return new Promise((resolve, reject) => {
         if (applicationFilePaths.length === 0) {
@@ -30,8 +34,8 @@ export function generateMacAppIcons(applicationFilePaths: string[]): Promise<voi
 function generateMacAppIcon(applicationFilePath: string): Promise<void> {
     return new Promise((resolve, reject) => {
         getPlistContent(applicationFilePath)
-            .then((data) => {
-                const icnsFilePath = getIcnsFilePath(applicationFilePath, data);
+            .then((plistContent) => {
+                const icnsFilePath = getIcnsFilePath(applicationFilePath, plistContent);
                 const outPngFilePath = getApplicationIconFilePath(applicationFilePath);
                 convertIcnsToPng(icnsFilePath, outPngFilePath)
                     .then(() => resolve())
@@ -45,7 +49,7 @@ function convertIcnsToPng(icnsFilePath: string, outFilePath: string): Promise<vo
     return executeCommand(`sips -s format png "${icnsFilePath}" --out "${outFilePath}"`);
 }
 
-function getIcnsFilePath(applicationFilePath: string, parsedPlistContent: any): string {
+function getIcnsFilePath(applicationFilePath: string, parsedPlistContent: InfoPlist | undefined): string {
     if (!parsedPlistContent) {
         return defaultIcnsFilePath;
     }
@@ -62,17 +66,20 @@ function getIcnsFilePath(applicationFilePath: string, parsedPlistContent: any): 
     }
 }
 
-function getPlistContent(applicationFilePath: string): Promise<any | undefined> {
-    return new Promise((resolve, reject) => {
+function getPlistContent(applicationFilePath: string): Promise<InfoPlist | undefined> {
+    return new Promise((resolve) => {
+        const onError = () => resolve(undefined);
+        const onSuccess = (data: InfoPlist) => resolve(data);
         const plistFilePath = join(applicationFilePath, "Contents", "Info.plist");
+
         if (!existsSync(plistFilePath)) {
-            resolve(undefined);
+            onError();
         } else {
-            plist.readFile(plistFilePath, (err: string, data: any) => {
+            plist.readFile(plistFilePath, (err, data) => {
                 if (err) {
-                    reject(err);
+                    onError()
                 } else {
-                    resolve(data);
+                    onSuccess(data);
                 }
             });
         }
