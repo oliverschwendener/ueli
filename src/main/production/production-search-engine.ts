baaa06689ada16161c3990dd7ce0ec61d1f6b2d9
Torben Kohlmeier <tkohlmeier@gmx.de>
2020-05-01 13:22:22 +0200

    log errors in searchWindowsApplications() instead of just ignoring them

--- src/main/production/production-search-engine.ts
+++ src/main/production/production-search-engine.ts
@@ -92,6 +92,7 @@ export function getProductionSearchEngine(config: UserConfigOptions, translation
                     logger,
                 ),
                 applicationSearcher,
+                logger,
             ),
             filePathExecutor,
             filePathLocationExecutor,
