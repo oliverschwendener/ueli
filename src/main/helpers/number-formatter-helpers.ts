5cfc76f3d5d4a8f5606b5d9c8858476bf981362d
themegaphoenix <themegaphoenix@users.noreply.github.com>
2020-05-11 10:27:05 +0100

    Improved performance with the formatting number: https://jsben.ch/umq99 https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript

--- src/main/helpers/number-formatter-helpers.ts
+++ src/main/helpers/number-formatter-helpers.ts
@@ -1,5 +1,3 @@
 export function formatNumberSeparator(numberToFormat:string, separator:string) {
-        const numParts = numberToFormat.split(".");
-        numParts[0] = numParts[0].replace(/\B(?=(\d{3})+(?!\d))/g, separator);
-        return numParts.join(".");
+        return numberToFormat.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, separator);
 }
