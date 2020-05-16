87514010fb352652d00bb192c18e154863e024ef
Amr Bashir <48618675+AmrBinBashir@users.noreply.github.com>
2020-05-16 03:23:59 +0200

    fix import color theme bug

--- src/common/helpers/color-theme-helpers.ts
+++ src/common/helpers/color-theme-helpers.ts
@@ -3,6 +3,7 @@ import { ColorThemeOptions } from "../config/color-theme-options";
 export function isValidColorTheme(colorThemeOptions: ColorThemeOptions): boolean {
     return Object
         .values(colorThemeOptions)
+        .filter((value, index) => index !== 0)
         .every((value) => isValidHexColor(value));
 }
 
