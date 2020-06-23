7ed191327de2ff4175708b98a76dcb5e4d512e02
Amr Bashir <48618675+AmrBinBashir@users.noreply.github.com>
2020-06-23 21:07:14 +0200

    generate windows icons without shortcut arrow

--- src/main/plugins/application-search-plugin/windows-app-icon-generator.ts
+++ src/main/plugins/application-search-plugin/windows-app-icon-generator.ts
@@ -50,12 +50,26 @@ function generateIcons(icons: Icon[], followShortcuts?: boolean): Promise<void> 
             Add-Type -AssemblyName System.Drawing;
             $iconsBase64 = "${iconsJsonBase64}";
             $iconsJson = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($iconsBase64));
+            $Shell = New-Object -ComObject WScript.Shell;
             $icons = $iconsJson | ConvertFrom-Json;
             $icons |
-                Where-Object { Test-Path -LiteralPath $_.inputFilePath } |
-                ForEach-Object {
+            Where-Object { Test-Path -LiteralPath $_.inputFilePath } |
+            ForEach-Object {
+                    $path;
+                    if($_.inputFilePath -match '.lnk$'){
+                        $path = $Shell.CreateShortcut($_.inputFilePath).targetpath;
+                        if($path -ne ""){
+                            if ((Test-Path -Path $path -PathType Container ) -or ($path -match '.url$')) {
+                                $path = $_.inputFilePath;
+                            }
+                        }else{
+                            $path = $_.inputFilePath;
+                        }
+                    }else{
+                        $path = $_.inputFilePath;
+                    }
                     $icon = $null;
-                    $icon = [System.Drawing.Icon]::ExtractAssociatedIcon($_.inputFilePath);
+                    $icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path);
                     if ($icon -ne $null) {
                         $outputFormat = $_.outputFormat;
                         $bitmap = $icon.ToBitmap().Save($_.outputFilePath, [System.Drawing.Imaging.ImageFormat]::$outputFormat);
