f4e3ce26cfa3559356bfc6b865361e73c0272f28
Amr Bashir <48618675+AmrBinBashir@users.noreply.github.com>
2020-06-24 13:16:18 +0200

    check if history is not empty

--- src/main/user-input-history-manager.ts
+++ src/main/user-input-history-manager.ts
@@ -8,7 +8,11 @@ export class UserInputHistoryManager {
     }
 
     public addItem(userInput: string): void {
-        if (this.history[this.history.length - 1] !== userInput) {
+        if (this.history.length > 0) {
+            if (this.history[this.history.length - 1] !== userInput) {
+                this.history.push(userInput);
+            }
+        } else {
             this.history.push(userInput);
         }
         this.index = this.history.length;
