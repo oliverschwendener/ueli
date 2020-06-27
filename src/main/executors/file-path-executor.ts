f74112c3006dac96c3a534d0d23c70bd254f4d21
sashaweiss <asashaweiss@gmail.com>
2020-06-27 10:06:30 -0700

    Always use Powershell/osascript to execute applications

--- src/main/executors/file-path-executor.ts
+++ src/main/executors/file-path-executor.ts
@@ -1,33 +1,17 @@
 import { executeCommand } from "./command-executor";
-import { shell } from "electron";
 
 export function executeFilePathWindows(filePath: string, privileged: boolean): Promise<void> {
-    return privileged
-        ? executeFilePathWindowsAsPrivileged(filePath)
-        : openFile(filePath);
+    const command: string = privileged ?
+        `powershell -Command "& {Start-Process -Verb runas '${filePath}'}"` :
+        `powershell -Command "& {Start-Process '${filePath}'}"`;
+
+    return executeCommand(command);
 }
 
 export function executeFilePathMacOs(filePath: string, privileged: boolean): Promise<void> {
-    return privileged
-        ? executeFilePathMacOsAsPrivileged(filePath)
-        : openFile(filePath);
-}
+    const command: string = privileged ?
+        `osascript -e 'do shell script "open \\"${filePath}\\"" with administrator privileges"'` :
+        `osascript -e 'do shell script "open \\"${filePath}\\""'`;
 
-function openFile(filePath: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-        const result = shell.openItem(filePath);
-        if (result) {
-            resolve();
-        } else {
-            reject(`Failed to open: ${filePath}`);
-        }
-    });
-}
-
-function executeFilePathWindowsAsPrivileged(filePath: string): Promise<void> {
-    return executeCommand(`powershell -Command "& {Start-Process -Verb runas '${filePath}'}"`);
-}
-
-function executeFilePathMacOsAsPrivileged(filePath: string): Promise<void> {
-    return executeCommand(`osascript -e 'do shell script "open \\"${filePath}\\"" with administrator privileges'`);
+    return executeCommand(command);
 }
