89d1c427888672988d8f38de6f4b3d20e9af956b
Michael Keck <me@michaelkeck.de>
2021-01-06 18:53:13 +0100

    Fixed Web Search encoding
    Merged pull-request #553

--- src/main/executors/websearch-suggestion-resolver.ts
+++ src/main/executors/websearch-suggestion-resolver.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export function getWebearchSuggestions(url: string): Promise<any> {
     return new Promise((resolve, reject) => {
-        axios.get(url)
+        axios.get(url, { headers: { "user-agent": "Mozilla/5.0" } })
             .then((response) => resolve(response.data))
             .catch((error) => reject(error));
     });
