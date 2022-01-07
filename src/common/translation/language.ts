6b992bb833b6766e47ddc8f282c2c2599df6e72e
Oliver Schwendener <oliver.schwendener@hotmail.com>
2022-01-07 23:16:57 +0100

    Added italian translation option

--- src/common/translation/language.ts
+++ src/common/translation/language.ts
@@ -1,12 +1,13 @@
 export enum Language {
+    Chinese = "简体中文",
+    Czech = "Česky",
     English = "English",
     German = "Deutsch",
+    Italian = "Italiano",
+    Japanese = "日本語",
+    Korean = "한국어",
     Portuguese = "Português",
     Russian = "Русский",
-    Czech = "Česky",
-    Turkish = "Türkçe",
     Spanish = "Español",
-    Chinese = "简体中文",
-    Korean = "한국어",
-    Japanese = "日本語",
+    Turkish = "Türkçe",
 }
--- src/common/translation/translation-set-manager.ts
+++ src/common/translation/translation-set-manager.ts
@@ -1,14 +1,15 @@
-import { TranslationSet } from "./translation-set";
-import { Language } from "./language";
+import { chineseTranslationSet } from "./chinese-translation-set";
 import { englishTranslationSet } from "./english-translation-set";
 import { germanTranslationSet } from "./german-translation-set";
-import { russianTranslationSet } from "./russian-translation-set";
-import { turkishTranslationSet } from "./turkish-translation-set";
-import { portugueseTranslationSet } from "./portuguese-translation-set";
-import { spanishTranslationSet } from "./spanish-translation-set";
-import { chineseTranslationSet } from "./chinese-translation-set";
-import { koreanTranslationSet } from "./korean-translation-set";
+import { italianTranslationSet } from "./italian-translation-set";
 import { japaneseTranslationSet } from "./japanese-translation-set";
+import { koreanTranslationSet } from "./korean-translation-set";
+import { Language } from "./language";
+import { portugueseTranslationSet } from "./portuguese-translation-set";
+import { russianTranslationSet } from "./russian-translation-set";
+import { spanishTranslationSet } from "./spanish-translation-set";
+import { TranslationSet } from "./translation-set";
+import { turkishTranslationSet } from "./turkish-translation-set";
 
 export function getTranslationSet(language: Language): TranslationSet {
     switch (language) {
@@ -30,6 +31,8 @@ export function getTranslationSet(language: Language): TranslationSet {
             return koreanTranslationSet;
         case Language.Japanese:
             return japaneseTranslationSet;
+        case Language.Italian:
+            return italianTranslationSet;
         default:
             return englishTranslationSet;
     }
