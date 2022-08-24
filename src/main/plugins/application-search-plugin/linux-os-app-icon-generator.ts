a39aa84f3858c480240bbba9e80fc5be7dcf69cf
Reza Jelveh <git@jelveh.me>
2022-08-25 01:00:11 +0200

    improve linux icon generation and system command

--- src/main/plugins/application-search-plugin/linux-os-app-icon-generator.ts
+++ src/main/plugins/application-search-plugin/linux-os-app-icon-generator.ts
@@ -1,8 +1,11 @@
-import { join } from "path";
+import { join, extname, basename } from "path";
 import { parse } from "ini"
+import { platform, release } from "os";
 import { FileHelpers } from "../../../common/helpers/file-helpers";
 import { applicationIconLocation, getApplicationIconFilePath } from "./application-icon-helpers";
 import { executeCommand, executeCommandWithOutput } from "../../executors/command-executor";
+import { getCurrentOperatingSystem, getOperatingSystemVersion } from "../../../common/helpers/operating-system-helpers";
+import { OperatingSystemVersion } from "../../../common/operating-system";
 
 export function generateLinuxAppIcons(applicationFilePaths: string[]): Promise<void> {
     return new Promise((resolve, reject) => {
@@ -17,8 +20,9 @@ export function generateLinuxAppIcons(applicationFilePaths: string[]): Promise<v
                 }
 
                 getIconThemeName().then((iconTheme) => {
-                    Promise.all([iconTheme, "hicolor"].map((t) => buildIconsList(t))).then((iconsLists) => {
-                        Promise.all(applicationFilePaths.map((application) => generateLinuxAppIcon(application, iconsLists[0], iconsLists[1])))
+                    Promise.all([iconTheme, "hicolor"].map((t) => buildIconsListFromTheme(t, 48)).concat(buildIconsListFromFolder("/usr/share/pixmap")))
+                    .then((iconsLists) => {
+                        Promise.all(applicationFilePaths.map((application) => generateLinuxAppIcon(application, iconsLists.flat())))
                             .then(() => resolve())
                             .catch((err) => reject(err));
                     }).catch((err) => reject(err));
@@ -27,31 +31,21 @@ export function generateLinuxAppIcons(applicationFilePaths: string[]): Promise<v
     });
 }
 
-// This written pretty awful and probably needs to be fixed
-function generateLinuxAppIcon(applicationFilePath: string, iconsList: string[], fallbackIconList: string[]): Promise<void> {
+// Doesn't follow freedesktop specs exactly but seems to work
+function generateLinuxAppIcon(applicationFilePath: string, iconsList: string[]): Promise<void> {
     return new Promise((resolve, reject) => {
         getAppIconName(applicationFilePath).then((iconName) => {
-            if (iconName.length != 0) {
+            if (iconName.length !== 0) {
                 const outSvgFilePath = getApplicationIconFilePath(applicationFilePath);
-                // I don't think resolve() breaks forEach, does it? Anyway it's wacky and hacky solution.
-                iconsList.forEach((iconPath) =>{
-                    if (iconPath.includes(`${iconName}.svg`) || iconPath.includes(`${iconName}-symbolic.svg`)) {
-                        convertSVGtoPng(iconPath, outSvgFilePath)
-                            .then(() => resolve())
-                            .catch((err) => reject(err));
-                    }
-                });
-                fallbackIconList.forEach((iconPath) =>{
-                    if (iconPath.includes(`${iconName}.svg`) || iconPath.includes(`${iconName}-symbolic.svg`)) {
-                        convertSVGtoPng(iconPath, outSvgFilePath)
-                            .then(() => resolve())
-                            .catch((err) => reject(err));
-                    }
-                });
-                console.error(`Failed to find icon ${iconName}.svg`);
+                const icon = iconsList.find((iconPath) => basename(iconPath, extname(iconPath)) === iconName);
+                if (icon) {
+                    convertSVGtoPng(icon, outSvgFilePath);
+                } else {
+                    console.error(`Failed to find icon ${iconName}.svg`)
+                };
                 resolve();
             } else {
-                console.error(`Failed to find icon for ${applicationFilePath}`);
+                console.error(`Failed to find icon name for ${applicationFilePath}`);
                 resolve();
             }
         }).catch((err) => reject(err));
@@ -74,11 +68,13 @@ function getAppIconName(applicationFilePath: string): Promise<string> {
 
 function getIconThemeName(): Promise<string> {
     return new Promise((resolve, reject) => {
-        FileHelpers.readFile("/usr/share/icons/default/index.theme").then((data) => {
-            const config = parse(data);
-            const iconThemeName = config["Icon Theme"]["Inherits"];
+        const desktopEnv = getOperatingSystemVersion(getCurrentOperatingSystem(platform()), release()) ===
+        // Only 2 DEs supported as of now (tested with Cinnamon so far)
+            OperatingSystemVersion.LinuxGnome ? "gnome" : "cinnamon";
+        executeCommandWithOutput(`gsettings get org.${desktopEnv}.desktop.interface icon-theme`)
+        .then((iconThemeName) => {
             if (iconThemeName) {
-                resolve(JSON.stringify(iconThemeName).slice(1, -1));
+                resolve(iconThemeName.slice(1, -2));
             } else {
                 reject("Failed to determine default icon theme");
             }
@@ -86,22 +82,53 @@ function getIconThemeName(): Promise<string> {
     });
 }
 
-function buildIconsList(iconTheme: string): Promise<string[]> {
+function buildIconsListFromTheme(iconTheme: string, size: number): Promise<string[]> {
     return new Promise((resolve, reject) => {
-        const iconThemePath = join("/usr/share/icons", iconTheme, "scalable");
+        const iconThemePath = join("/usr/share/icons", iconTheme);
 
-        executeCommandWithOutput(`find ${iconThemePath}`).then((output) => {
-            const iconFiles = output.split("\n");
-            executeCommandWithOutput("find /usr/share/pixmaps").then((output) => {
-                const pixelmapFiles = output.split("\n");
-                resolve(iconFiles.concat(pixelmapFiles));
-            }).catch((err) => reject(err));
+        let iconList : Promise<string>[] = [];
+        const nonIdealIconList : Promise<string>[] = [];
+
+        FileHelpers.readFile(`${iconThemePath}/index.theme`).then((data) => {
+            const themeCofig = parse(data);
+            const subdirs : string[] = themeCofig["Icon Theme"]["Directories"].split(",");
+            subdirs.reverse().forEach((subdir) => {
+                if (Number(themeCofig[subdir]["Size"]) === size) {
+                    // Prioritize icons with ideal size
+                    iconList.push(executeCommandWithOutput(`find '${join(iconThemePath, subdir)}'`));
+                } else {
+                    nonIdealIconList.push(executeCommandWithOutput(`find '${join(iconThemePath, subdir)}'`));
+                }
+            });
+
+            Promise.allSettled(iconList.concat(nonIdealIconList)).then((icons) => {
+                let results : string[] = [];
+                icons.forEach((result) => {
+                    if (result.status === "fulfilled") {
+                        results = results.concat(result.value.split("\n").filter(file => [".png", ".svg", ".xpm"].includes(extname(file))));
+                    } else {
+                        console.error(result.reason);
+                    }
+                })
+                resolve(results);
+            })
+            .catch((err) => reject(err));
+
+        })
+        .catch((err) => reject(`Failed to read index.theme for ${iconTheme}`));
+    });
+}
+
+function buildIconsListFromFolder(folder: string): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+        executeCommandWithOutput("find /usr/share/pixmaps").then((pixOutput) => {
+            const pixelmapFiles = pixOutput.split("\n");
+            resolve(pixelmapFiles);
         }).catch((err) => reject(err));
-
     });
 }
 
 function convertSVGtoPng(source: string, output: string): Promise<void>{
     console.debug(`Converting ${source} to ${output}`);
-    return executeCommand(`convert -background none -density 1000 -resize 256x ${source} ${output}`);
+    return executeCommand(`convert -background none -resize 32x ${source} ${output}`);
 }
--- src/main/plugins/operating-system-commands-plugin/linux-operating-system-command-repository.ts
+++ src/main/plugins/operating-system-commands-plugin/linux-operating-system-command-repository.ts
@@ -1,8 +1,11 @@
 import { OperatingSystemCommand } from "./operating-system-command";
+import { platform, release } from "os";
 import { IconType } from "../../../common/icon/icon-type";
 import { OperatingSystemCommandRepository } from "./operating-system-commands-repository";
 import { TranslationSet } from "../../../common/translation/translation-set";
 import { UserConfigOptions } from "../../../common/config/user-config-options";
+import { OperatingSystemVersion } from "../../../common/operating-system";
+import { getCurrentOperatingSystem, getOperatingSystemVersion } from "../../../common/helpers/operating-system-helpers";
 
 export class LinuxOperatingSystemCommandRepository implements OperatingSystemCommandRepository {
     private translationSet: TranslationSet;
@@ -25,13 +28,18 @@ export class LinuxOperatingSystemCommandRepository implements OperatingSystemCom
     }
 
     private getOperatingSystemCommands(): OperatingSystemCommand[] {
-        // const systemCommandMapping = {
-        //     [OperatingSystemVersion.LinuxGnome]: {
-        //         lockscreen: `dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock`,
-        //         logout: `dbus-send --session --type=method_call --print-reply --dest=org.gnome.SessionManager /org/gnome/SessionManager org.gnome.SessionManager.Logout uint32:1`
-        //     }
-        // }
-        // const version = getLinuxVersion()
+        const systemCommandMapping : { [key: string]: { [key: string]: string} } = {
+            [OperatingSystemVersion.LinuxGnome]: {
+                lockscreen: `dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock`,
+                logout: `dbus-send --session --type=method_call --print-reply --dest=org.gnome.SessionManager /org/gnome/SessionManager org.gnome.SessionManager.Logout uint32:1`
+            },
+            [OperatingSystemVersion.LinuxCinnamon]: {
+                lockscreen: `cinnamon-screensaver-command --lock`,
+                logout: `cinnamon-session-quit --logout`,
+            }
+        }
+        const version = getOperatingSystemVersion(getCurrentOperatingSystem(platform()), release());
+        if (!systemCommandMapping[version]) throw new Error(`Unsupported Linux desktop environment: ${version}`);
         return [
             {
                 description: this.translationSet.linuxShutdownDescription,
@@ -57,40 +65,40 @@ export class LinuxOperatingSystemCommandRepository implements OperatingSystemCom
                 name: this.translationSet.linuxRestart,
                 searchable: [this.translationSet.linuxRestart],
             },
-            // {
-            //     description: this.translationSet.linuxLogoutDescription,
-            //     executionArgument: `logout`,
-            //     icon: {
-            //         parameter: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 26 26" version="1.1">
-            //         <g id="surface1">
-            //             <path d="M 6 0 C 4.34375 0 3 1.34375 3 3 L 3 23 C 3 24.65625 4.34375 26 6 26 L 20 26 C 21.65625 26 23 24.65625 23 23 L 23 17.8125 L 21 19.5625 L 21 23 C 21 23.550781 20.550781 24 20 24 L 6 24 C 5.449219 24 5 23.550781 5 23 L 5 3 C 5 2.449219 5.449219 2 6 2 L 20 2 C 20.550781 2 21 2.449219 21 3 L 21 6.4375 L 23 8.1875 L 23 3 C 23 1.34375 21.65625 0 20 0 Z M 16.5 6.46875 C 16.25 6.542969 16 6.796875 16 7.46875 L 16 10 L 10 10 C 9.449219 10 9 10.449219 9 11 L 9 15 C 9 15.550781 9.449219 16 10 16 L 16 16 L 16 18.5 C 16 19.78125 17 19.5 17 19.5 L 24.5 13 L 17 6.5 C 17 6.5 16.75 6.394531 16.5 6.46875 Z "></path>
-            //         </g>
-            //     </svg>`,
-            //         type: IconType.SVG,
-            //     },
-            //     name: this.translationSet.linuxLogout,
-            //     searchable: [this.translationSet.linuxLogout],
-            // },
-            // {
-            //     description: this.translationSet.linuxSleepDescription,
-            //     executionArgument: `systemctl suspend`,
-            //     icon: {
-            //         parameter: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M 5 4 L 5 6 L 13.15625 6 L 5.3125 13.75 L 5 14.0625 L 5 16 L 16 16 L 16 14 L 7.90625 14 L 15.6875 6.3125 L 16 6.03125 L 16 4 Z M 18 10 L 18 12 L 24.15625 12 L 18.3125 17.75 L 18 18.0625 L 18 20 L 27 20 L 27 18 L 20.90625 18 L 26.6875 12.3125 L 27 12.03125 L 27 10 Z M 8 19 L 8 21 L 13.15625 21 L 8.3125 25.75 L 8 26.0625 L 8 28 L 16 28 L 16 26 L 10.90625 26 L 15.6875 21.3125 L 16 21.03125 L 16 19 Z"></path></svg>`,
-            //         type: IconType.SVG,
-            //     },
-            //     name: this.translationSet.linuxSleep,
-            //     searchable: [this.translationSet.linuxSleep],
-            // },
-            // {
-            //     description: this.translationSet.linuxLockDescription,
-            //     executionArgument: `xdg-screensaver lock`,
-            //     icon: {
-            //         parameter: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" version="1.1"><g id="surface1"><path d="M 16 3 C 12.15625 3 9 6.15625 9 10 L 9 13 L 6 13 L 6 29 L 26 29 L 26 13 L 23 13 L 23 10 C 23 6.15625 19.84375 3 16 3 Z M 16 5 C 18.753906 5 21 7.246094 21 10 L 21 13 L 11 13 L 11 10 C 11 7.246094 13.246094 5 16 5 Z M 8 15 L 24 15 L 24 27 L 8 27 Z "></path></g></svg>`,
-            //         type: IconType.SVG,
-            //     },
-            //     name: this.translationSet.linuxLock,
-            //     searchable: [this.translationSet.linuxLock],
-            // },
+            {
+                description: this.translationSet.linuxLogoutDescription,
+                executionArgument: systemCommandMapping[version]["logout"],
+                icon: {
+                    parameter: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 26 26" version="1.1">
+                    <g id="surface1">
+                        <path d="M 6 0 C 4.34375 0 3 1.34375 3 3 L 3 23 C 3 24.65625 4.34375 26 6 26 L 20 26 C 21.65625 26 23 24.65625 23 23 L 23 17.8125 L 21 19.5625 L 21 23 C 21 23.550781 20.550781 24 20 24 L 6 24 C 5.449219 24 5 23.550781 5 23 L 5 3 C 5 2.449219 5.449219 2 6 2 L 20 2 C 20.550781 2 21 2.449219 21 3 L 21 6.4375 L 23 8.1875 L 23 3 C 23 1.34375 21.65625 0 20 0 Z M 16.5 6.46875 C 16.25 6.542969 16 6.796875 16 7.46875 L 16 10 L 10 10 C 9.449219 10 9 10.449219 9 11 L 9 15 C 9 15.550781 9.449219 16 10 16 L 16 16 L 16 18.5 C 16 19.78125 17 19.5 17 19.5 L 24.5 13 L 17 6.5 C 17 6.5 16.75 6.394531 16.5 6.46875 Z "></path>
+                    </g>
+                </svg>`,
+                    type: IconType.SVG,
+                },
+                name: this.translationSet.linuxLogout,
+                searchable: [this.translationSet.linuxLogout],
+            },
+            {
+                description: this.translationSet.linuxSleepDescription,
+                executionArgument: `systemctl suspend`,
+                icon: {
+                    parameter: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M 5 4 L 5 6 L 13.15625 6 L 5.3125 13.75 L 5 14.0625 L 5 16 L 16 16 L 16 14 L 7.90625 14 L 15.6875 6.3125 L 16 6.03125 L 16 4 Z M 18 10 L 18 12 L 24.15625 12 L 18.3125 17.75 L 18 18.0625 L 18 20 L 27 20 L 27 18 L 20.90625 18 L 26.6875 12.3125 L 27 12.03125 L 27 10 Z M 8 19 L 8 21 L 13.15625 21 L 8.3125 25.75 L 8 26.0625 L 8 28 L 16 28 L 16 26 L 10.90625 26 L 15.6875 21.3125 L 16 21.03125 L 16 19 Z"></path></svg>`,
+                    type: IconType.SVG,
+                },
+                name: this.translationSet.linuxSleep,
+                searchable: [this.translationSet.linuxSleep],
+            },
+            {
+                description: this.translationSet.linuxLockDescription,
+                executionArgument: systemCommandMapping[version]["lockscreen"],
+                icon: {
+                    parameter: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" version="1.1"><g id="surface1"><path d="M 16 3 C 12.15625 3 9 6.15625 9 10 L 9 13 L 6 13 L 6 29 L 26 29 L 26 13 L 23 13 L 23 10 C 23 6.15625 19.84375 3 16 3 Z M 16 5 C 18.753906 5 21 7.246094 21 10 L 21 13 L 11 13 L 11 10 C 11 7.246094 13.246094 5 16 5 Z M 8 15 L 24 15 L 24 27 L 8 27 Z "></path></g></svg>`,
+                    type: IconType.SVG,
+                },
+                name: this.translationSet.linuxLock,
+                searchable: [this.translationSet.linuxLock],
+            },
         ];
     }
 }
