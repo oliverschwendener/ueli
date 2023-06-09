5b5fbc32b8deba2fbc14c8b936881205e8d7aca1
Oliver Schwendener <oliver.schwendener@hotmail.com>
2023-06-09 11:15:52 +0200

    Refactoring

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -1,15 +1,22 @@
 import { app, BrowserWindow, ipcMain, IpcMainEvent, nativeTheme } from "electron";
 import { join } from "path";
 
-const createBrowserWindow = (appIsPackaged: boolean): BrowserWindow => {
-    const preloadScriptFilePath = appIsPackaged
+const getPreloadScriptFilePath = (appIsPackaged: boolean) =>
+    appIsPackaged
         ? join(__dirname, "..", "..", "dist-electron", "preload", "index.js")
         : join(__dirname, "..", "preload", "index.js");
 
+const loadFileOrUrl = (browserWindow: BrowserWindow, appIsPackaged: boolean) => {
+    appIsPackaged
+        ? browserWindow.loadFile(join(__dirname, "..", "..", "dist", "index.html"))
+        : browserWindow.loadURL(process.env.VITE_DEV_SERVER_URL);
+};
+
+const createBrowserWindow = (appIsPackaged: boolean): BrowserWindow => {
     return new BrowserWindow({
         autoHideMenuBar: true,
         webPreferences: {
-            preload: preloadScriptFilePath,
+            preload: getPreloadScriptFilePath(appIsPackaged),
         },
         vibrancy: "window",
         backgroundMaterial: "auto",
@@ -18,28 +25,19 @@ const createBrowserWindow = (appIsPackaged: boolean): BrowserWindow => {
     });
 };
 
-const loadFileOrUrl = (browserWindow: BrowserWindow, appIsPackaged: boolean) => {
-    appIsPackaged
-        ? browserWindow.loadFile(join(__dirname, "..", "..", "dist", "index.html"))
-        : browserWindow.loadURL(process.env.VITE_DEV_SERVER_URL);
-};
-
 const registerIpcEventListeners = (browserWindow: BrowserWindow) => {
-    ipcMain.on("themeShouldUseDarkColors", (event: IpcMainEvent) => {
-        event.returnValue = nativeTheme.shouldUseDarkColors;
-    });
+    ipcMain.on(
+        "themeShouldUseDarkColors",
+        (event: IpcMainEvent) => (event.returnValue = nativeTheme.shouldUseDarkColors),
+    );
 
     ipcMain.on("settingsOpenStateChanged", (_, { settingsOpened }: { settingsOpened: boolean }) =>
-        browserWindow.setBounds({ height: settingsOpened ? 600 : 60 })
+        browserWindow.setBounds({ height: settingsOpened ? 600 : 60 }),
     );
 };
 
-const registerNativeThemeEventListeners = (allBrowserWindows: BrowserWindow[]) => {
-    nativeTheme.addListener("updated", () => {
-        for (const browserWindow of allBrowserWindows) {
-            browserWindow.webContents.send("nativeThemeChanged");
-        }
-    });
+const registerNativeThemeEventListeners = (browserWindow: BrowserWindow) => {
+    nativeTheme.addListener("updated", () => browserWindow.webContents.send("nativeThemeChanged"));
 };
 
 (async () => {
@@ -47,5 +45,5 @@ const registerNativeThemeEventListeners = (allBrowserWindows: BrowserWindow[]) =
     const browserWindow = createBrowserWindow(app.isPackaged);
     loadFileOrUrl(browserWindow, app.isPackaged);
     registerIpcEventListeners(browserWindow);
-    registerNativeThemeEventListeners(BrowserWindow.getAllWindows());
+    registerNativeThemeEventListeners(browserWindow);
 })();
