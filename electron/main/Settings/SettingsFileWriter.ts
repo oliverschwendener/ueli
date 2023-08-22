63b548e019d33e2ee2dad83f75760a191c56df54
Oliver Schwendener <oliver.schwendener@proton.me>
2023-08-22 12:13:51 +0200

    Sort json keys alphabetically

--- electron/main/Settings/SettingsFileWriter.ts
+++ electron/main/Settings/SettingsFileWriter.ts
@@ -7,9 +7,22 @@ export class SettingsFileWriter implements SettingsWriter {
 
     public writeSettings(settings: Settings): Promise<void> {
         return new Promise((resolve, reject) => {
-            writeFile(this.settingsFilePath, JSON.stringify(settings), "utf-8", (error) => {
-                error ? reject(error) : resolve();
-            });
+            writeFile(
+                this.settingsFilePath,
+                JSON.stringify(SettingsFileWriter.sortSettingsAlphabetically(settings), null, 4),
+                "utf-8",
+                (error) => (error ? reject(error) : resolve()),
+            );
         });
+    }
+
+    private static sortSettingsAlphabetically(settings: Settings): Settings {
+        const result: Settings = {};
+
+        for (const key of Object.keys(settings).sort()) {
+            result[key] = settings[key];
+        }
+
+        return result;
     }
 }
