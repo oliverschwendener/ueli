067210dc06fbdb6afed789c977e0588ea0c08556
Oliver Schwendener <oliver.schwendener@hotmail.com>
2023-09-22 16:56:42 +0200

    Added test for PowershellExecutionService

---
+++ electron/main/Executor/ExecutionServices/PowershellExecutionService.test.ts
@@ -0,0 +1,26 @@
+import type { ExecutionArgument } from "@common/ExecutionArgument";
+import { describe, expect, it, vi } from "vitest";
+import type { CommandlineUtility } from "../../Utilities";
+import { PowershellExecutionService } from "./PowershellExecutionService";
+
+describe(PowershellExecutionService, () => {
+    it("should execute powershell commands", () => {
+        const executeCommandMock = vi.fn();
+
+        const commandlineUtility = <CommandlineUtility>{
+            executeCommand: (command) => executeCommandMock(command),
+        };
+
+        const executionArgument = <ExecutionArgument>{
+            searchResultItem: {
+                executionServiceArgument: "this is my powershell command",
+            },
+        };
+
+        new PowershellExecutionService(commandlineUtility).execute(executionArgument);
+
+        expect(executeCommandMock).toHaveBeenCalledWith(
+            `powershell -Command "& {${executionArgument.searchResultItem.executionServiceArgument}}"`,
+        );
+    });
+});
