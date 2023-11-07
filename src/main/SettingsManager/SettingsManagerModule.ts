6f5fa170c03c8f4db92008e2797fbe4e2c6b9b24
Oliver Schwendener <oliver.schwendener@proton.me>
2023-11-07 16:33:01 +0100

    Removed unnecessary module

--- src/main/SettingsManager/SettingsManagerModule.ts
+++ src/main/SettingsManager/SettingsManagerModule.ts
@@ -2,6 +2,7 @@ import type { DependencyInjector } from "@common/DependencyInjector";
 import type { SettingsManager as SettingsManagerInterface } from "@common/SettingsManager";
 import type { SettingsReader } from "@common/SettingsReader";
 import type { SettingsWriter } from "@common/SettingsWriter";
+import type { IpcMain } from "electron";
 import { SettingsManager } from "./SettingsManager";
 
 export class SettingsManagerModule {
@@ -12,6 +13,23 @@ export class SettingsManagerModule {
         dependencyInjector.registerInstance<SettingsManagerInterface>(
             "SettingsManager",
             new SettingsManager(settingsReader, settingsWriter),
+        );
+
+        SettingsManagerModule.registerIpcEvents(dependencyInjector);
+    }
+
+    private static registerIpcEvents(dependencyInjector: DependencyInjector): void {
+        const settingsManager = dependencyInjector.getInstance<SettingsManager>("SettingsManager");
+        const ipcMain = dependencyInjector.getInstance<IpcMain>("IpcMain");
+
+        ipcMain.handle("updateSettingByKey", (_, { key, value }: { key: string; value: unknown }) =>
+            settingsManager.saveSetting(key, value),
+        );
+
+        ipcMain.on(
+            "getSettingByKey",
+            (event, { key, defaultValue }: { key: string; defaultValue: unknown }) =>
+                (event.returnValue = settingsManager.getSettingByKey(key, defaultValue)),
         );
     }
 }
