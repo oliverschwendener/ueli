a7dd74934d360a9d3747501b26116f24122b36d8
Oliver Schwendener <oliver.schwendener@proton.me>
2023-11-29 12:24:31 +0100

    Added type

--- src/main/FileSystemUtility/FileSystemUtilityModule.ts
+++ src/main/FileSystemUtility/FileSystemUtilityModule.ts
@@ -1,8 +1,9 @@
 import type { DependencyInjector } from "../DependencyInjector";
+import type { FileSystemUtility } from "./Contract";
 import { NodeJsFileSystemUtility } from "./NodeJsFileSystemUtility";
 
 export class FileSystemUtilityModule {
     public static bootstrap(dependencyInjector: DependencyInjector) {
-        dependencyInjector.registerInstance("FileSystemUtility", new NodeJsFileSystemUtility());
+        dependencyInjector.registerInstance<FileSystemUtility>("FileSystemUtility", new NodeJsFileSystemUtility());
     }
 }
