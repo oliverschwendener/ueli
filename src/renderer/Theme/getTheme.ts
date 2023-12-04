17500df57eb6982ede89c96ecc435688aacda28c
Oliver Schwendener <oliver.schwendener@proton.me>
2023-12-04 14:04:57 +0100

    Fixed bug

--- src/renderer/Theme/getTheme.ts
+++ src/renderer/Theme/getTheme.ts
@@ -3,6 +3,6 @@ import type { Theme } from "@fluentui/react-components";
 import { themeMap } from "./themes";
 
 export const getTheme = (contextBridge: ContextBridge): Theme => {
-    const themeName = contextBridge.getSettingByKey<string>("appearance.preferredThemeName", "Ueli");
+    const themeName = contextBridge.getSettingByKey<string>("appearance.themeName", "Ueli");
     return contextBridge.themeShouldUseDarkColors() ? themeMap[themeName].dark : themeMap[themeName].light;
 };
