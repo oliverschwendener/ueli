1d6034434fae1e822f5048545820219c95643f45
Oliver Schwendener <oliver.schwendener@proton.me>
2023-12-20 14:15:07 +0100

    Simplified code

--- src/main/Extensions/SystemColorThemeSwitcher/SystemColorThemeSwitcherModule.ts
+++ src/main/Extensions/SystemColorThemeSwitcher/SystemColorThemeSwitcherModule.ts
@@ -7,23 +7,13 @@ import { SystemColorThemeSwitcher } from "./SystemColorThemeSwitcher";
 
 export class SystemColorThemeSwitcherModule {
     public static bootstrap(dependencyInjector: DependencyInjector) {
-        SystemColorThemeSwitcherModule.registerCustomActionHandler(dependencyInjector);
-        SystemColorThemeSwitcherModule.registerExtension(dependencyInjector);
-    }
-
-    private static registerCustomActionHandler(dependencyInjector: DependencyInjector) {
         const operatingSystem = dependencyInjector.getInstance<OperatingSystem>("OperatingSystem");
         const commandlineUtility = dependencyInjector.getInstance<CommandlineUtility>("CommandlineUtility");
         const nativeTheme = dependencyInjector.getInstance<NativeTheme>("NativeTheme");
 
+        dependencyInjector.registerExtension(new SystemColorThemeSwitcher(operatingSystem));
         dependencyInjector.registerActionHandler(
             new CustomActionHandler(operatingSystem, commandlineUtility, nativeTheme),
         );
-    }
-
-    private static registerExtension(dependencyInjector: DependencyInjector) {
-        const operatingSystem = dependencyInjector.getInstance<OperatingSystem>("OperatingSystem");
-
-        dependencyInjector.registerExtension(new SystemColorThemeSwitcher(operatingSystem));
     }
 }
