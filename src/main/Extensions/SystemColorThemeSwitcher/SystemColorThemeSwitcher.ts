c4fe6dc48332e79d9f57f3252b7e52fdf438b4cc
Oliver Schwendener <oliver.schwendener@proton.me>
2024-01-12 10:30:49 +0100

    Added new static icons for SystemColorThemeSwitcher

--- src/main/Extensions/SystemColorThemeSwitcher/SystemColorThemeSwitcher.ts
+++ src/main/Extensions/SystemColorThemeSwitcher/SystemColorThemeSwitcher.ts
@@ -2,6 +2,7 @@ import type { OperatingSystem } from "@common/OperatingSystem";
 import type { SearchResultItem } from "@common/SearchResultItem";
 import type { DependencyInjector } from "@Core/DependencyInjector";
 import type { Extension } from "@Core/Extension";
+import type { ExtensionAssetPathResolver } from "@Core/ExtensionAssets";
 
 export class SystemColorThemeSwitcher implements Extension {
     public readonly id: string = "SystemColorThemeSwitcher";
@@ -9,7 +10,10 @@ export class SystemColorThemeSwitcher implements Extension {
     public readonly nameTranslationKey: string = "extension[SystemColorThemeSwitcher].extensionName";
     public readonly supportedOperatingSystems: OperatingSystem[] = ["Windows", "macOS"];
 
-    public constructor(private readonly currentOperatingSystem: OperatingSystem) {}
+    public constructor(
+        private readonly currentOperatingSystem: OperatingSystem,
+        private readonly extensionAssetPathResolver: ExtensionAssetPathResolver,
+    ) {}
 
     public async getSearchResultItems(): Promise<SearchResultItem[]> {
         return [
@@ -18,7 +22,10 @@ export class SystemColorThemeSwitcher implements Extension {
                 id: "SystemColorThemeSwitcher:toggle",
                 name: "Toggle System Appearance",
                 nameTranslationKey: "",
-                imageUrl: this.getSearchResultItemImageUrl(),
+                imageUrl: `file://${this.extensionAssetPathResolver.getAssetFilePath(
+                    this.id,
+                    this.getSearchResultItemFileName(),
+                )}`,
                 defaultAction: {
                     argument: "toggle",
                     description: "",
@@ -40,11 +47,10 @@ export class SystemColorThemeSwitcher implements Extension {
         return supportedOperatingSystems.includes(currentOperatingSystem);
     }
 
-    private getSearchResultItemImageUrl(): string {
+    private getSearchResultItemFileName(): string {
         return {
-            macOS: "https://upload.wikimedia.org/wikipedia/en/thumb/2/23/System_Preferences_icon.png/120px-System_Preferences_icon.png",
-            Windows:
-                "https://preview.redd.it/windows-11-logo-in-svg-format-v0-sudz5o3s1vn91.png?width=1080&format=png&auto=webp&s=196ef4f2bff864c6d3f58b074fa32479a285ab49",
+            macOS: "macos-system-settings-icon.png",
+            Windows: "windows-11-logo.webp",
         }[this.currentOperatingSystem];
     }
 }
--- src/main/Extensions/SystemColorThemeSwitcher/SystemColorThemeSwitcherModule.ts
+++ src/main/Extensions/SystemColorThemeSwitcher/SystemColorThemeSwitcherModule.ts
@@ -1,5 +1,6 @@
 import type { CommandlineUtility } from "@Core/CommandlineUtility";
 import type { DependencyInjector } from "@Core/DependencyInjector";
+import type { ExtensionAssetPathResolver } from "@Core/ExtensionAssets";
 import type { OperatingSystem } from "@common/OperatingSystem";
 import type { NativeTheme } from "electron";
 import { CustomActionHandler } from "./CustomActionHandler";
@@ -10,8 +11,10 @@ export class SystemColorThemeSwitcherModule {
         const operatingSystem = dependencyInjector.getInstance<OperatingSystem>("OperatingSystem");
         const commandlineUtility = dependencyInjector.getInstance<CommandlineUtility>("CommandlineUtility");
         const nativeTheme = dependencyInjector.getInstance<NativeTheme>("NativeTheme");
+        const extensionAssetPathResolver =
+            dependencyInjector.getInstance<ExtensionAssetPathResolver>("ExtensionAssetPathResolver");
 
-        dependencyInjector.registerExtension(new SystemColorThemeSwitcher(operatingSystem));
+        dependencyInjector.registerExtension(new SystemColorThemeSwitcher(operatingSystem, extensionAssetPathResolver));
         dependencyInjector.registerActionHandler(
             new CustomActionHandler(operatingSystem, commandlineUtility, nativeTheme),
         );
