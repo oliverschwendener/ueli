138e1c2b4feebb027d5ec1e3915bbb6bb02db09d
Torben Kohlmeier <tkohlmeier@gmx.de>
2024-01-27 12:59:08 +0100

    updated translation plugin to use v2 linguee api

--- src/main/plugins/translation-plugin/linguee-translator.ts
+++ src/main/plugins/translation-plugin/linguee-translator.ts
@@ -1,37 +1,20 @@
 import axios from "axios";
 
-interface WordType {
+interface Translation {
+    text: string;
     pos: string;
 }
 
-interface Translation {
-    text: string;
-    word_type: WordType;
-}
-
-interface ExactMatch {
+interface Lemma {
     translations: Translation[];
-}
-
-interface TranslationResponse {
-    exact_matches?: ExactMatch[];
 }
 
 export class LingueeTranslator {
     public static getTranslations(url: string): Promise<Translation[]> {
         return new Promise((resolve, reject) => {
             axios
-                .get(url)
-                .then((response) => {
-                    const data = response.data as TranslationResponse;
-                    let translations: Translation[] = [];
-                    if (data.exact_matches) {
-                        data.exact_matches
-                            .map((exactMatch) => exactMatch.translations)
-                            .forEach((t) => (translations = translations.concat(t)));
-                    }
-                    resolve(translations);
-                })
+                .get<Lemma[]>(url)
+                .then((response) => resolve(response.data?.flatMap((l) => l.translations) ?? []))
                 .catch((err) => reject(err));
         });
     }
