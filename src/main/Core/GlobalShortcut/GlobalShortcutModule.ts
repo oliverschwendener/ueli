aac2bf855c848e9c511d34a780063f3b3bc39681
Oliver Schwendener <oliver.schwendener@proton.me>
2024-02-08 15:51:48 +0100

    Only register hotkey when it's valid

--- src/main/Core/GlobalShortcut/GlobalShortcutModule.ts
+++ src/main/Core/GlobalShortcut/GlobalShortcutModule.ts
@@ -1,5 +1,6 @@
 import type { Dependencies } from "@Core/Dependencies";
 import type { DependencyRegistry } from "@Core/DependencyRegistry";
+import { isValidHotkey } from "@common/Core/Hotkey";
 
 export class GlobalShortcutModule {
     public static bootstrap(dependencyRegistry: DependencyRegistry<Dependencies>) {
@@ -7,9 +8,14 @@ export class GlobalShortcutModule {
         const eventEmitter = dependencyRegistry.get("EventEmitter");
         const eventSubscriber = dependencyRegistry.get("EventSubscriber");
         const settingsManager = dependencyRegistry.get("SettingsManager");
+        const logger = dependencyRegistry.get("Logger");
 
         const registerHotkey = () => {
             const hotkey = settingsManager.getValue("general.hotkey", "Alt+Space");
+
+            if (!isValidHotkey(hotkey)) {
+                logger.error(`Unable to register hotkey. Reason: unexpected hotkey ${hotkey}`);
+            }
 
             globalShortcut.unregisterAll();
             globalShortcut.register(hotkey, () => eventEmitter.emitEvent("hotkeyPressed"));
