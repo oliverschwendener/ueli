e1869919e6018e9101c7a73071b07f63dba607f0
Oliver Schwendener <oliver.schwendener@proton.me>
2024-03-05 16:15:14 +0100

    Added descriptions for AssetPathResolver module

--- src/main/Core/AssetPathResolver/Contract/AssetPathResolver.ts
+++ src/main/Core/AssetPathResolver/Contract/AssetPathResolver.ts
@@ -1,4 +1,20 @@
+/**
+ * Module to resolve asset paths for modules and extensions.
+ */
 export interface AssetPathResolver {
+    /**
+     * Resolves the path to an asset file for a Core module.
+     * @param moduleId The ID of the module, e.g.: "BrowserWindow"
+     * @param assetFileName The name of the asset file, e.g.: "icon.png"
+     * @returns The resolved path to the asset file, e.g.: "C:\example\BrowserWindow\icon.png"
+     */
     getModuleAssetPath(moduleId: string, assetFileName: string): string;
+
+    /**
+     * Resolves the path to an asset file for an extension.
+     * @param extensionId The ID of the extension, e.g.: "MyCustomExtension"
+     * @param assetFileName The name of the asset file, e.g.: "icon.png"
+     * @returns The resolved path to the asset file, e.g.: "C:\example\MyCustomExtension\icon.png"
+     */
     getExtensionAssetPath(extensionId: string, assetFileName: string): string;
 }
