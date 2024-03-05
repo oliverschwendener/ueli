7f093653a6cc41f87d5334006d7db1535301ffac
Oliver Schwendener <oliver.schwendener@hotmail.com>
2024-03-05 17:38:51 +0100

    Added more comments

--- src/main/Core/ImageGenerator/Contract/FileImageGenerator.ts
+++ src/main/Core/ImageGenerator/Contract/FileImageGenerator.ts
@@ -1,6 +1,22 @@
 import type { Image } from "@common/Core/Image";
 
+/**
+ * Module for generating images from files.
+ */
 export interface FileImageGenerator {
+    /**
+     * Get an image from a file path.
+     * @param filePath The file path, e.g.: `"C:/path/to/image.png"`.
+     */
     getImage(filePath: string): Promise<Image>;
+
+    /**
+     * Gets images from file paths.
+     * @param filePaths The file paths, e.g.: `["C:/path/to/image1.png", "C:/path/to/image2.png"]`.
+     * @returns A promise that resolves to a record of images, where the key is the file path.
+     *
+     * Silently fails if it fails to generate an image for one of the file paths. The failing file path wont be included
+     * in the resulting record.
+     */
     getImages(filePaths: string[]): Promise<Record<string, Image>>;
 }
--- src/main/Core/ImageGenerator/Contract/UrlImageGenerator.ts
+++ src/main/Core/ImageGenerator/Contract/UrlImageGenerator.ts
@@ -1,5 +1,12 @@
 import type { Image } from "@common/Core/Image";
 
+/**
+ * Module for generating images from URLs.
+ */
 export interface UrlImageGenerator {
+    /**
+     * Gets an image from a URL.
+     * @param url The URL, e.g.: `"https://example.com"`.
+     */
     getImage(url: string): Image;
 }
