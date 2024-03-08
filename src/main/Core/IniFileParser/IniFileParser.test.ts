88e71be3068dab64be68872547ce19520a3cf8c7
Oliver Schwendener <oliver.schwendener@hotmail.com>
2024-03-08 11:45:09 +0100

    Use ini npm package to parse ini files

--- src/main/Core/IniFileParser/IniFileParser.test.ts
+++ src/main/Core/IniFileParser/IniFileParser.test.ts
@@ -4,18 +4,34 @@ import { IniFileParser } from "./IniFileParser";
 describe(IniFileParser, () => {
     it("should parse an ini file", () => {
         const iniFileString = `
-# Comment
+; This comment is being ignored
+scope = global
 
-[Group1]
-key1=value1
+[database]
+user = dbuser
+password = dbpassword
+database = use_this_database
 
-[Group2]
-key2=value2
+[paths.default]
+datadir = /var/lib/data
+array[] = first value
+array[] = second value
+array[] = third value
         `;
 
-        expect(new IniFileParser().parseIniFileContent(iniFileString)).toEqual({
-            Group1: { key1: "value1" },
-            Group2: { key2: "value2" },
+        expect(new IniFileParser().parseIniFileContent(iniFileString.trim())).toEqual({
+            database: {
+                database: "use_this_database",
+                password: "dbpassword",
+                user: "dbuser",
+            },
+            paths: {
+                default: {
+                    array: ["first value", "second value", "third value"],
+                    datadir: "/var/lib/data",
+                },
+            },
+            scope: "global",
         });
     });
 });
