a8b50385ec02269986b949a639ad3c88194d2c4b
Oliver Schwendener <oliver.schwendener@hotmail.com>
2024-03-10 14:47:15 +0100

    Removed ini dependency

--- src/main/Core/IniFileParser/IniFileParser.test.ts
+++ src/main/Core/IniFileParser/IniFileParser.test.ts
@@ -4,7 +4,7 @@ import { IniFileParser } from "./IniFileParser";
 describe(IniFileParser, () => {
     it("should parse an ini file", () => {
         const iniFileString = `
-; This comment is being ignored
+# This comment is being ignored
 scope = global
 
 [database]
@@ -14,24 +14,20 @@ database = use_this_database
 
 [paths.default]
 datadir = /var/lib/data
-array[] = first value
-array[] = second value
-array[] = third value
         `;
 
         expect(new IniFileParser().parseIniFileContent(iniFileString.trim())).toEqual({
+            "": {
+                scope: "global",
+            },
             database: {
-                database: "use_this_database",
-                password: "dbpassword",
                 user: "dbuser",
+                password: "dbpassword",
+                database: "use_this_database",
             },
-            paths: {
-                default: {
-                    array: ["first value", "second value", "third value"],
-                    datadir: "/var/lib/data",
-                },
+            "paths.default": {
+                datadir: "/var/lib/data",
             },
-            scope: "global",
         });
     });
 });
--- src/main/Core/IniFileParser/IniFileParser.ts
+++ src/main/Core/IniFileParser/IniFileParser.ts
@@ -1,8 +1,37 @@
-import { parse } from "ini";
 import { IniFileParser as IniFileParserInterface } from "./Contract";
 
 export class IniFileParser implements IniFileParserInterface {
     public parseIniFileContent(fileString: string): Record<string, unknown> {
-        return parse(fileString);
+        const fileContent = fileString.split("\n").filter((v) => !v.startsWith("#") && v !== "");
+        const entries: Record<string, Record<string, string>> = {};
+        let group = "";
+        entries[group] = {};
+        for (const line of fileContent) {
+            // Comments
+            if (line.startsWith("#") || line.trim() === "") {
+                continue;
+            }
+
+            // New group
+            if (line.startsWith("[") && line.endsWith("]") && !line.slice(1, -1).match(/\[\]/m)) {
+                if (entries[line]) {
+                    throw `Ini Format Error! Duplicate groups found in ${fileString}`;
+                }
+
+                group = line.slice(1, -1);
+                entries[group] = {};
+            } else {
+                // Entry
+                const [key, ...entrySplit] = line.split("=");
+                const entry = entrySplit.join("=");
+
+                if (entries[group][key.trim()]) {
+                    throw `Ini Format Error! Duplicate key in ${group} in file ${fileString}`;
+                }
+
+                entries[group][key.trim()] = entry.trim();
+            }
+        }
+        return entries;
     }
 }
