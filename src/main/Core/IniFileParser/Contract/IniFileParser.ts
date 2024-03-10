f67d9e0a4e131754046f27da1c80df3fc3d5edb8
Oliver Schwendener <oliver.schwendener@hotmail.com>
2024-03-10 14:49:45 +0100

    Added description

--- src/main/Core/IniFileParser/Contract/IniFileParser.ts
+++ src/main/Core/IniFileParser/Contract/IniFileParser.ts
@@ -1,3 +1,17 @@
+/**
+ * Offers methods to parse an ini file content.
+ */
 export interface IniFileParser {
+    /**
+     * Parses the given ini file content and returns the result as an object.
+     * @param fileString The ini file content, e.g.:
+     * ```ini
+     * scope = global
+     * [database]
+     * nuser = dbuser
+     * password = dbpassword
+     * ```
+     * @returns The parsed ini file content as an object. Global entries are stored in the empty string key.
+     */
     parseIniFileContent(fileString: string): Record<string, unknown>;
 }
