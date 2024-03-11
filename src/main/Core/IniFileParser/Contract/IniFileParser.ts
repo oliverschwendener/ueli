902b2a30c562682c9fb9eb328fc1b8e848334b2d
Kelvin <kelvinw138@gmail.com>
2024-03-11 06:29:08 -0700

    Removed types

--- src/main/Core/IniFileParser/Contract/IniFileParser.ts
+++ src/main/Core/IniFileParser/Contract/IniFileParser.ts
@@ -4,15 +4,15 @@
 export interface IniFileParser {
     /**
      * Parses the given ini file content and returns the result as an object.
-     * @param {string} fileString The ini file content, e.g.:
+     * @param fileString The ini file content, e.g.:
      * ```ini
      * scope = global
      * [database]
      * nuser = dbuser
      * password = dbpassword
      * ```
-     * @param {string} [commentDelimiter=';'] The symbol used to indicate comments
-     * @param {boolean} [allowInlineComments] Whether inline comments are parsed
+     * @param [commentDelimiter=';'] The symbol used to indicate comments
+     * @param [allowInlineComments] Whether inline comments are parsed
      * @returns The parsed ini file content as an object. Global entries are stored in the empty string key.
      */
     parseIniFileContent(fileString: string, commentDelimiter?: string, allowInlineComments?: boolean): Record<string, unknown>;
