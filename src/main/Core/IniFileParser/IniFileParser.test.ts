331bcac49b3a676abbb0b5b17190c5f2d9dd15b1
ke1v <67396422+ke1v@users.noreply.github.com>
2024-03-11 05:50:40 -0700

    Feature: Configurable IniFileParser (#1079)
    * Improved IniFileParser
    
    * Fixed bug in IniFileParser

--- src/main/Core/IniFileParser/IniFileParser.test.ts
+++ src/main/Core/IniFileParser/IniFileParser.test.ts
@@ -4,8 +4,9 @@ import { IniFileParser } from "./IniFileParser";
 describe(IniFileParser, () => {
     it("should parse an ini file", () => {
         const iniFileString = `
-# This comment is being ignored
+; This comment is being ignored
 scope = global
+comments = no;inline;comments
 
 [database]
 user = dbuser
@@ -19,11 +20,45 @@ datadir = /var/lib/data
         expect(new IniFileParser().parseIniFileContent(iniFileString.trim())).toEqual({
             "": {
                 scope: "global",
+                comments: "no;inline;comments",
             },
             database: {
                 user: "dbuser",
                 password: "dbpassword",
                 database: "use_this_database",
+            },
+            "paths.default": {
+                datadir: "/var/lib/data",
+            },
+        });
+    });
+
+    it("should parse an ini file with a different comment delimiter and inline comments", () => {
+        const iniFileString = `
+# This comment is being ignored
+scope = global
+notcomment = this;is;not;a;comment
+iscomment = value#an inline comment
+
+[database]
+user = dbuser
+password = dbpassword
+database = use_this_database
+
+[paths.default]
+datadir = /var/lib/data
+        `;
+
+        expect(new IniFileParser().parseIniFileContent(iniFileString.trim(), "#", true)).toEqual({
+            "": {
+                scope: "global",
+                notcomment: "this;is;not;a;comment",
+                iscomment: "value",
+            },
+            database: {
+                database: "use_this_database",
+                password: "dbpassword",
+                user: "dbuser",
             },
             "paths.default": {
                 datadir: "/var/lib/data",
