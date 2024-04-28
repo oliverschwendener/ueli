27115a3ed4aa9558a3c82936e20e7377658bcdfb
Oliver Schwendener <oliver.schwendener@hotmail.com>
2024-04-28 15:18:49 +0200

    Disallow omission of curly braces
    Additionally formatted code in IniFileParser to make it easier to read

--- src/main/Core/IniFileParser/IniFileParser.ts
+++ src/main/Core/IniFileParser/IniFileParser.ts
@@ -10,27 +10,37 @@ export class IniFileParser implements IniFileParserInterface {
         let fileLines = fileString.split(/(\r\n|\r|\n)/);
 
         fileLines = fileLines.filter((line) => !line.startsWith(commentDelimiter) && line.trim() !== "");
+
         if (allowInlineComments) {
             fileLines = fileLines.map((line) => {
                 const index = line.indexOf(commentDelimiter);
-                return index >= 0 ? line.slice(0, line.indexOf(commentDelimiter)) : line;
+                return index >= 0 ? line.slice(0, index) : line;
             });
         }
 
         const entries: Record<string, Record<string, string>> = {};
+
         let group = "";
         entries[group] = {};
+
         for (const line of fileLines) {
             // New group
             if (line.startsWith("[") && line.endsWith("]") && !line.slice(1, -1).match(/\[\]/m)) {
-                if (entries[line]) throw `Ini Format Error! Duplicate groups found in ${fileString}`;
+                if (entries[line]) {
+                    throw `Ini Format Error! Duplicate groups found in ${fileString}`;
+                }
+
                 group = line.slice(1, -1);
                 entries[group] = {};
             } else {
                 // Entry
                 const [key, ...entrySplit] = line.split("=").map((text) => text.trim());
                 const entry = entrySplit.join("=");
-                if (entries[group][key]) throw `Ini Format Error! Duplicate key in ${group} in file ${fileString}`;
+
+                if (entries[group][key]) {
+                    throw `Ini Format Error! Duplicate key in ${group} in file ${fileString}`;
+                }
+
                 entries[group][key] = entry;
             }
         }
