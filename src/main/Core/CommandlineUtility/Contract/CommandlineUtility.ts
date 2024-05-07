78dbda4d069d2d6af518a38435965a570f770aa3
Oliver Schwendener <oliver.schwendener@proton.me>
2024-05-07 17:05:47 +0200

    Added missing comment

--- src/main/Core/CommandlineUtility/Contract/CommandlineUtility.ts
+++ src/main/Core/CommandlineUtility/Contract/CommandlineUtility.ts
@@ -5,8 +5,8 @@ export interface CommandlineUtility {
     /**
      * Executes a command and returns the output.
      * @param command The command to execute, e.g. `"echo Hello World"`.
-     * @param ignoreStdErr If `true`, the standard error output is ignored and not thrown as an error.
      * @param ignoreErr If `true`, the error is ignored and not thrown as an error.
+     * @param ignoreStdErr If `true`, the standard error output is ignored and not thrown as an error.
      * @returns A promise that resolves with the output of the command.
      */
     executeCommand(command: string, ignoreStdErr?: boolean, ignoreErr?: boolean): Promise<string>;
