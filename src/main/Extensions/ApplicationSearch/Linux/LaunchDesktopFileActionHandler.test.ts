3d07190141a52d4303488b4569dfd70e3297e447
Oliver Schwendener <oliver.schwendener@proton.me>
2024-05-11 17:18:27 +0200

    Simplified code

--- src/main/Extensions/ApplicationSearch/Linux/LaunchDesktopFileActionHandler.test.ts
+++ src/main/Extensions/ApplicationSearch/Linux/LaunchDesktopFileActionHandler.test.ts
@@ -1,19 +1,17 @@
 import type { CommandlineUtility } from "@Core/CommandlineUtility";
-import { EnvironmentVariableProvider } from "@Core/EnvironmentVariableProvider";
+import type { EnvironmentVariableProvider } from "@Core/EnvironmentVariableProvider";
 import type { SearchResultItemAction } from "@common/Core";
 import { describe, expect, it, vi } from "vitest";
 import { LaunchDesktopFileActionHandler } from "./LaunchDesktopFileActionHandler";
 
 describe(LaunchDesktopFileActionHandler, () => {
     it("should call `gio launch` when executing search result item with the argument file name on GNOME", async () => {
-        const executeCommandMock = vi.fn().mockReturnValue(Promise.resolve(""));
+        const executeCommandMock = vi.fn().mockResolvedValue("");
         const commandlineUtility = <CommandlineUtility>{ executeCommand: (path) => executeCommandMock(path) };
 
-        const getEnvMock = vi.fn().mockReturnValue("GNOME");
-        const getAllEnvMock = vi.fn().mockReturnValue({ XDG_CURRENT_DESKTOP: "GNOME" });
         const environmentVariableProvider = <EnvironmentVariableProvider>{
-            get: getEnvMock,
-            getAll: getAllEnvMock,
+            get: () => "GNOME",
+            getAll: () => ({}),
         };
 
         const actionHandler = new LaunchDesktopFileActionHandler(commandlineUtility, environmentVariableProvider);
@@ -27,14 +25,12 @@ describe(LaunchDesktopFileActionHandler, () => {
     });
 
     it("should call `kde-open` when executing search result item with the argument file name on KDE", async () => {
-        const executeCommandMock = vi.fn().mockReturnValue(Promise.resolve(""));
+        const executeCommandMock = vi.fn().mockResolvedValue("");
         const commandlineUtility = <CommandlineUtility>{ executeCommand: (path) => executeCommandMock(path) };
 
-        const getEnvMock = vi.fn().mockReturnValue("KDE");
-        const getAllEnvMock = vi.fn().mockReturnValue({ XDG_CURRENT_DESKTOP: "KDE" });
         const environmentVariableProvider = <EnvironmentVariableProvider>{
-            get: getEnvMock,
-            getAll: getAllEnvMock,
+            get: () => "KDE",
+            getAll: () => ({}),
         };
 
         const actionHandler = new LaunchDesktopFileActionHandler(commandlineUtility, environmentVariableProvider);
@@ -48,14 +44,12 @@ describe(LaunchDesktopFileActionHandler, () => {
     });
 
     it("should use the first desktop environment compatible found when provided with multiple options", async () => {
-        const executeCommandMock = vi.fn().mockReturnValue(Promise.resolve(""));
+        const executeCommandMock = vi.fn().mockResolvedValue("");
         const commandlineUtility = <CommandlineUtility>{ executeCommand: (path) => executeCommandMock(path) };
 
-        const getEnvMock = vi.fn().mockReturnValue("ubuntu:GNOME:XFCE:KDE");
-        const getAllEnvMock = vi.fn().mockReturnValue({ XDG_CURRENT_DESKTOP: "GNOME:XFCE:KDE" });
         const environmentVariableProvider = <EnvironmentVariableProvider>{
-            get: getEnvMock,
-            getAll: getAllEnvMock,
+            get: () => "ubuntu:GNOME:XFCE:KDE",
+            getAll: () => ({}),
         };
 
         const actionHandler = new LaunchDesktopFileActionHandler(commandlineUtility, environmentVariableProvider);
@@ -69,28 +63,21 @@ describe(LaunchDesktopFileActionHandler, () => {
     });
 
     it("should throw an error if `gio launch` throws an error", async () => {
-        const executeCommandMock = vi
-            .fn()
-            .mockReturnValue(
-                Promise.reject(
-                    "gio: Unable to load '/usr/share/applications/firefox.desktop': No such file or directory",
-                ),
-            );
+        const errorMessage = "gio: Unable to load '/usr/share/applications/firefox.desktop': No such file or directory";
+
+        const executeCommandMock = vi.fn().mockRejectedValue(errorMessage);
+
         const commandlineUtility = <CommandlineUtility>{ executeCommand: (path) => executeCommandMock(path) };
 
-        const getEnvMock = vi.fn().mockReturnValue("GNOME");
-        const getAllEnvMock = vi.fn().mockReturnValue({ XDG_CURRENT_DESKTOP: "GNOME" });
         const environmentVariableProvider = <EnvironmentVariableProvider>{
-            get: getEnvMock,
-            getAll: getAllEnvMock,
+            get: () => "GNOME",
+            getAll: () => ({}),
         };
 
         const actionHandler = new LaunchDesktopFileActionHandler(commandlineUtility, environmentVariableProvider);
 
         await expect(
             actionHandler.invokeAction(<SearchResultItemAction>{ argument: "/usr/share/applications/firefox.desktop" }),
-        ).rejects.toThrowError(
-            "gio: Unable to load '/usr/share/applications/firefox.desktop': No such file or directory",
-        );
+        ).rejects.toThrowError(errorMessage);
     });
 });
