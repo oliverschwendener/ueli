7ffb0096c12aeb3e24038275877389d8b91ade4c
Oliver Schwendener <oliver.schwendener@proton.me>
2024-05-13 18:28:23 +0200

    Added test for RandomStringProvider

---
+++ src/main/Core/RandomStringProvider/RandomStringProvider.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from "vitest";
+import { RandomStringProvider } from "./RandomStringProvider";
+
+describe(RandomStringProvider, () => {
+    describe(RandomStringProvider.prototype.getRandomUUid, () => {
+        it("it should not return the same string twice", () => {
+            const randomStringProvider = new RandomStringProvider();
+            expect(randomStringProvider.getRandomUUid()).not.toEqual(randomStringProvider.getRandomUUid());
+        });
+    });
+});
