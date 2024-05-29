3283ecac72a17b83111c5baf81bfcb62b7bfe36f
Oliver Schwendener <oliver.schwendener@proton.me>
2024-05-29 17:25:07 +0200

    feat(BrowserBookmarks): support multiple browsers at the same time

--- src/renderer/Extensions/BrowserBookmarks/BrowserBookmarksSettings.tsx
+++ src/renderer/Extensions/BrowserBookmarks/BrowserBookmarksSettings.tsx
@@ -12,7 +12,7 @@ export const BrowserBookmarksSettings = () => {
 
     const extensionId = "BrowserBookmarks";
 
-    const browsers: Browser[] = [
+    const browserOptions: Browser[] = [
         "Arc",
         "Brave Browser",
         "Firefox",
@@ -23,23 +23,32 @@ export const BrowserBookmarksSettings = () => {
 
     const searchResultStyles = ["nameOnly", "urlOnly", "nameAndUrl"];
 
-    const { value: browser, updateValue: setBrowser } = useExtensionSetting<Browser>({ extensionId, key: "browser" });
+    const { value: browsers, updateValue: setBrowsers } = useExtensionSetting<Browser[]>({
+        extensionId,
+        key: "browsers",
+    });
 
     const { value: searchResultStyle, updateValue: setSearchResultStyle } = useExtensionSetting<string>({
         extensionId,
         key: "searchResultStyle",
     });
 
+    const { value: iconType, updateValue: setIconType } = useExtensionSetting<string>({
+        extensionId,
+        key: "iconType",
+    });
+
     return (
         <SectionList>
             <Section>
-                <Field label="Browser">
+                <Field label="Browsers">
                     <Dropdown
-                        value={`${browser}`}
-                        selectedOptions={[browser]}
-                        onOptionSelect={(_, { optionValue }) => optionValue && setBrowser(optionValue as Browser)}
+                        value={browsers.join(", ")}
+                        selectedOptions={browsers}
+                        onOptionSelect={(_, { selectedOptions }) => setBrowsers(selectedOptions as Browser[])}
+                        multiselect
                     >
-                        {browsers.map((browserName) => (
+                        {browserOptions.map((browserName) => (
                             <Option key={browserName} value={browserName} text={browserName}>
                                 <img
                                     style={{ width: 20, height: 20 }}
@@ -67,7 +76,18 @@ export const BrowserBookmarksSettings = () => {
                     </Dropdown>
                 </Field>
             </Section>
-            <Section></Section>
+            <Section>
+                <Field label="Icon Type">
+                    <Dropdown
+                        value={t(`iconType.${iconType}`, { ns })}
+                        selectedOptions={[iconType]}
+                        onOptionSelect={(_, { optionValue }) => optionValue && setIconType(optionValue)}
+                    >
+                        <Option value="favicon">{t("iconType.favicon", { ns })}</Option>
+                        <Option value="browserIcon">{t("iconType.browserIcon", { ns })}</Option>
+                    </Dropdown>
+                </Field>
+            </Section>
         </SectionList>
     );
 };
