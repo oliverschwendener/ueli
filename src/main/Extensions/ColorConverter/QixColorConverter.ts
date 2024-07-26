ce1909fb77a76b99e9a7e51b5ac2509dac7e9a8e
Oliver Schwendener <oliver.schwendener@proton.me>
2024-07-26 12:34:28 +0200

    Simplified code

--- src/main/Extensions/ColorConverter/QixColorConverter.ts
+++ src/main/Extensions/ColorConverter/QixColorConverter.ts
@@ -6,15 +6,13 @@ export class QixColorConverter implements ColorConverter {
     public convertFromString(value: string): ColorConversionResult[] {
         const color = this.extractColorFromString(value);
 
-        if (!color) {
-            return [];
-        }
-
-        return [
-            { format: "HEX", value: color.hex() },
-            { format: "HSL", value: color.hsl().string() },
-            { format: "RGB", value: color.rgb().string() },
-        ];
+        return color
+            ? [
+                  { format: "HEX", value: color.hex() },
+                  { format: "HSL", value: color.hsl().string() },
+                  { format: "RGB", value: color.rgb().string() },
+              ]
+            : [];
     }
 
     private extractColorFromString(value: string): Color | undefined {
