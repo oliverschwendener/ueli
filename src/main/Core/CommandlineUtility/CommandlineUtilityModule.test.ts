10dc336c7516304aeaab1875666c7f0cc46b26a8
Oliver Schwendener <oliver.schwendener@proton.me>
2024-08-25 11:52:38 +0200

    Fixed test

--- src/main/Core/CommandlineUtility/CommandlineUtilityModule.test.ts
+++ src/main/Core/CommandlineUtility/CommandlineUtilityModule.test.ts
@@ -1,18 +1,31 @@
+import type { ActionHandlerRegistry } from "@Core/ActionHandler";
 import type { Dependencies } from "@Core/Dependencies";
 import type { DependencyRegistry } from "@Core/DependencyRegistry";
 import { describe, expect, it, vi } from "vitest";
+import { CommandlineActionHandler } from "./ActionHandler";
 import { CommandlineUtilityModule } from "./CommandlineUtilityModule";
 import { NodeJsCommandlineUtility } from "./NodeJsCommandlineUtility";
 
 describe(CommandlineUtilityModule, () => {
     it("should register the NodeJsCommandlineUtility", () => {
+        const actionHandlerRegistryMock = <ActionHandlerRegistry>{
+            register: vi.fn(),
+            getAll: vi.fn(),
+            getById: vi.fn(),
+        };
+
         const dependencyRegistry = <DependencyRegistry<Dependencies>>{
-            get: vi.fn(),
+            get: vi.fn().mockReturnValue(actionHandlerRegistryMock),
             register: vi.fn(),
         };
 
         CommandlineUtilityModule.bootstrap(dependencyRegistry);
 
         expect(dependencyRegistry.register).toHaveBeenCalledWith("CommandlineUtility", new NodeJsCommandlineUtility());
+
+        expect(dependencyRegistry.get).toBeCalledWith("ActionHandlerRegistry");
+        expect(actionHandlerRegistryMock.register).toHaveBeenCalledWith(
+            new CommandlineActionHandler(new NodeJsCommandlineUtility()),
+        );
     });
 });
