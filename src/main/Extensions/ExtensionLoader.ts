4379cb9fab5d203df26d4d3dcf01afd124df827a
Oliver Schwendener <oliver.schwendener@proton.me>
2024-09-02 20:38:48 +0200

    fixed typo

--- src/main/Extensions/ExtensionLoader.ts
+++ src/main/Extensions/ExtensionLoader.ts
@@ -21,7 +21,7 @@ import { WorkflowExtensionModule } from "./Workflow";
 export class ExtensionLoader {
     private static getAllExtensionModules(): ExtensionModule[] {
         /**
-         * Here is where all extensions are loaded. Add you extension to this list. Make sure that the items in this
+         * Here is where all extensions are loaded. Add your extension to this list. Make sure that the items in this
          * list are alphabetically ordered.
          */
         return [
