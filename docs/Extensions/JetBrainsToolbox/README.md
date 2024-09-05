83b28be331cee23f507139437a840dfcfa495a69
Sebastian Comans <631504+scomans@users.noreply.github.com>
2024-09-05 14:00:07 +0200

    Add JetBrains-Toolbox extension

---
+++ docs/Extensions/JetBrainsToolbox/README.md
@@ -0,0 +1,18 @@
+# JetBrains Toolbox Extension
+
+This extension allows you to open recently edited projects with the respective JetBrains IDE.
+
+![Example](example.png)
+
+## About this extension
+
+Author: [Sebastian Comans](https://github.com/scomans)
+
+Supported operating systems:
+
+-   Windows
+-   macOS
+-   Linux
+
+> [!IMPORTANT]
+> Requires "Generate shell scripts" to be enable in JetBrains Toolbox
---
+++ src/main/Extensions/JetBrainsToolbox/JetBrainsToolboxModule.ts
@@ -0,0 +1,19 @@
+import type { Dependencies } from "@Core/Dependencies";
+import type { DependencyRegistry } from "@Core/DependencyRegistry";
+import type { ExtensionBootstrapResult } from "../ExtensionBootstrapResult";
+import type { ExtensionModule } from "../ExtensionModule";
+import { JetBrainsToolboxExtension } from "./JetBrainsToolboxExtension";
+
+export class JetBrainsToolboxModule implements ExtensionModule {
+    public bootstrap(dependencyRegistry: DependencyRegistry<Dependencies>): ExtensionBootstrapResult {
+        return {
+            extension: new JetBrainsToolboxExtension(
+                dependencyRegistry.get("OperatingSystem"),
+                dependencyRegistry.get("AssetPathResolver"),
+                dependencyRegistry.get("SettingsManager"),
+                dependencyRegistry.get("FileSystemUtility"),
+                dependencyRegistry.get("Translator"),
+            ),
+        };
+    }
+}
