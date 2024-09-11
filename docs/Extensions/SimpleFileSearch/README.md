8c0d9c2d030252628284b3b41e6ed2d22aaba20b
Oliver Schwendener <oliver.schwendener@hotmail.com>
2024-09-11 20:18:42 +0200

    feat: added simple file search extension (#1194)
    * wip: simple file search
    
    * More stuff
    
    * Added translations
    
    * Removed redundant additional actions
    
    * Moved createT() outside of loop
    
    * Simplified code
    
    * Removed todos
    
    * Added basic settings
    
    * Added more translations
    
    * Removed unnecessary `continue`
    
    * Restructured UI
    
    * Added translations
    
    * Extracted FolderSetting into separate file
    
    * Added icon for linux
    
    * Stuff
    
    * Removed edit button
    
    * Added ability to define option search for
    
    * Added option to only search for files or folder or both
    
    * Added translations
    
    * Added new icon
    
    * Added docs

---
+++ docs/Extensions/SimpleFileSearch/README.md
@@ -0,0 +1,26 @@
+# Simple File Search
+
+This extension allows you to search for files and folders in specific locations.
+
+![example](example.png)
+
+> Note that this extension is not made for scanning top-level folders, hence the term "simple".
+
+## Settings
+
+-   Path: the path to the folder in which you want to search for files and folders
+-   Search for:
+    -   Files and folders: select this option if you want to search for files and folders
+    -   Files: select this option if you want to search for files only
+    -   Folders: select this option if you want to search for folders only
+-   Recursive: whether to do a recursive scan. Note that if you enable this option on a folder that contains a lot of files, you might experience poor performance or even brake the app.
+
+## About this extension
+
+Author: [Oliver Schwendener](https://github.com/oliverschwendener)
+
+Supported operating systems:
+
+-   Windows
+-   macOS
+-   Linux
---
+++ src/main/Extensions/SimpleFileSearch/SimpleFileSearchExtensionModule.ts
@@ -0,0 +1,21 @@
+import type { Dependencies } from "@Core/Dependencies";
+import type { DependencyRegistry } from "@Core/DependencyRegistry";
+import type { ExtensionBootstrapResult } from "../ExtensionBootstrapResult";
+import type { ExtensionModule } from "../ExtensionModule";
+import { SimpleFileSearchExtension } from "./SimpleFileSearchExtension";
+
+export class SimpleFileSearchExtensionModule implements ExtensionModule {
+    public bootstrap(dependencyRegistry: DependencyRegistry<Dependencies>): ExtensionBootstrapResult {
+        return {
+            extension: new SimpleFileSearchExtension(
+                dependencyRegistry.get("FileSystemUtility"),
+                dependencyRegistry.get("FileImageGenerator"),
+                dependencyRegistry.get("Logger"),
+                dependencyRegistry.get("AssetPathResolver"),
+                dependencyRegistry.get("OperatingSystem"),
+                dependencyRegistry.get("SettingsManager"),
+                dependencyRegistry.get("Translator"),
+            ),
+        };
+    }
+}
