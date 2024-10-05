1545c51273755340b14c5ca9c8138953ed70a322
Benjamin Kraatz <benjaminkraatz@yahoo.com>
2024-10-05 15:43:46 +0200

    refactor: move toggleAppIconInDock function
    into bootstap as inlined anonymous function

--- src/main/Core/Dock/DockModule.ts
+++ src/main/Core/Dock/DockModule.ts
@@ -5,20 +5,18 @@ export class DockModule {
     public static bootstrap(dependencyRegistry: DependencyRegistry<Dependencies>) {
         const app = dependencyRegistry.get("App");
         const settingsManager = dependencyRegistry.get("SettingsManager");
-        const showAppIconInDock = settingsManager.getValue("appearance.showAppIconInDock", false);
-        this.toggleAppIconInDock(app, showAppIconInDock);
-
         const eventSubscriber = dependencyRegistry.get("EventSubscriber");
-        eventSubscriber.subscribe("settingUpdated[appearance.showAppIconInDock]", ({ value }: { value: boolean }) => {
-            this.toggleAppIconInDock(app, value);
-        });
-    }
 
-    private static toggleAppIconInDock(app: Electron.App, showAppIconInDock: boolean) {
-        if (showAppIconInDock) {
-            app.dock?.show();
-        } else {
-            app.dock?.hide();
-        }
+        const toggleAppIconInDock = () => {
+            const showAppIconInDock = settingsManager.getValue("appearance.showAppIconInDock", false);
+            if (showAppIconInDock) {
+                app.dock?.show();
+            } else {
+                app.dock?.hide();
+            }
+        };
+        toggleAppIconInDock();
+
+        eventSubscriber.subscribe("settingUpdated[appearance.showAppIconInDock]", toggleAppIconInDock);
     }
 }
