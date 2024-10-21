d758697bb988a48fe9e7ec6fc66e4dfb99d67e22
Oliver Schwendener <oliver.schwendener@proton.me>
2024-10-21 16:08:06 +0200

    Refactored LinuxApplicationRepository

--- src/main/Extensions/ApplicationSearch/Linux/LinuxApplicationRepository.ts
+++ src/main/Extensions/ApplicationSearch/Linux/LinuxApplicationRepository.ts
@@ -23,44 +23,73 @@ export class LinuxApplicationRepository implements ApplicationRepository {
     ) {}
 
     public async getApplications(): Promise<Application[]> {
-        const linuxFolders = this.settings.getValue<string[]>("linuxFolders");
+        const folderPaths = this.settings
+            .getValue<string[]>("linuxFolders")
+            .filter((folderPath) => this.fileSystemUtility.isDirectory(folderPath));
 
-        const filePromises = linuxFolders.map(async (folderPath) => {
-            if (!this.fileSystemUtility.isDirectory(folderPath)) {
-                throw `${folderPath} doesn't exist or isn't a folder.`;
-            }
-            return (await this.fileSystemUtility.readDirectory(folderPath)).filter(
-                (file) => extname(file) === ".desktop",
-            );
-        });
+        const filePaths = await this.getApplicationFilePaths(folderPaths);
+        const appIcons = await this.fileImageGenerator.getImages(filePaths);
 
-        const files = (await Promise.allSettled(filePromises))
-            .flatMap((result) => {
-                if (result.status === "rejected") {
-                    this.logger.warn(result.reason);
-                    return null;
-                }
-                return result.value;
-            })
-            .filter((value) => value);
-
-        return (await Promise.allSettled(files.map((file) => this.generateLinuxApplication(file))))
-            .map((result, index) => {
-                if (result.status === "rejected") {
-                    this.logger.warn(`Unable to generate Application for ${files[index]}. Reason: ${result.reason}`);
-                    return null;
-                }
-                return result.value;
-            })
-            .filter((value) => value);
+        return await this.generateLinuxApplications(filePaths, appIcons);
     }
 
-    private async generateLinuxApplication(filePath: string): Promise<LinuxApplication> {
+    private async getApplicationFilePaths(folderPaths: string[]): Promise<string[]> {
+        const readDirectoryPromiseResults = await Promise.allSettled(
+            folderPaths.map((folderPath) => this.fileSystemUtility.readDirectory(folderPath)),
+        );
+
+        const result: string[] = [];
+
+        for (const readDirectoryPromiseResult of readDirectoryPromiseResults) {
+            if (readDirectoryPromiseResult.status === "fulfilled") {
+                result.push(...readDirectoryPromiseResult.value.filter((filePath) => extname(filePath) === ".desktop"));
+            } else {
+                this.logger.warn(readDirectoryPromiseResult.reason);
+                continue;
+            }
+        }
+
+        return result;
+    }
+
+    private async generateLinuxApplications(
+        filePaths: string[],
+        appIcons: Record<string, Image>,
+    ): Promise<LinuxApplication[]> {
+        const applicationPromiseResults = await Promise.allSettled(
+            filePaths.map((filePath) =>
+                this.generateLinuxApplication(filePath, appIcons[filePath] ?? this.getDefaultAppIcon()),
+            ),
+        );
+
+        const applications: LinuxApplication[] = [];
+
+        for (let i = 0; i < applicationPromiseResults.length; i++) {
+            const filePath = filePaths[i];
+            const promiseReuslt = applicationPromiseResults[i];
+
+            if (promiseReuslt.status === "fulfilled") {
+                applications.push(promiseReuslt.value);
+            } else {
+                this.logger.warn(`Unable to generate Application for ${filePath}. Reason: ${promiseReuslt.reason}`);
+                continue;
+            }
+        }
+
+        return applications;
+    }
+
+    private async generateLinuxApplication(filePath: string, appIcon: Image): Promise<LinuxApplication> {
         const config: Record<string, string> = this.iniParser.parseIniFileContent(
             (await this.fileSystemUtility.readFile(filePath)).toString(),
         )["Desktop Entry"];
         // https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html#recognized-keys
         const desktopEnv = this.environmentVariableProvider.get("ORIGINAL_XDG_CURRENT_DESKTOP")?.split(":");
+
+        if (!desktopEnv) {
+            return Promise.reject("Unable to resolve desktop environment");
+        }
+
         if (
             !config ||
             !config["Icon"] ||
@@ -68,18 +97,13 @@ export class LinuxApplicationRepository implements ApplicationRepository {
             (config.OnlyShowIn !== undefined && !config.OnlyShowIn.split(";").some((i) => desktopEnv.includes(i))) ||
             (config.NotShowIn !== undefined && config.NotShowIn.split(";").some((i) => desktopEnv.includes(i)))
         ) {
-            return null;
+            return Promise.reject("Unexpected .desktop file");
         }
 
-        return new LinuxApplication(config["Name"], filePath, await this.getAppIcon(filePath));
+        return new LinuxApplication(config["Name"], filePath, appIcon);
     }
 
-    private async getAppIcon(filePath: string): Promise<Image> {
-        try {
-            return await this.fileImageGenerator.getImage(filePath);
-        } catch (error) {
-            this.logger.error(`Failed to find icon for ${filePath}. Reason: ${error}. Falling back to generic icon`);
-        }
+    private getDefaultAppIcon(): Image {
         return {
             url: `file://${this.assetPathResolver.getExtensionAssetPath("ApplicationSearch", "linux-generic-app-icon.png")}`,
         };
