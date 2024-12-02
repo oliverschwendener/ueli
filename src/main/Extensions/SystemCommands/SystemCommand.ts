5f43c843f7dba41a044b44d438816410f7cfbd32
Oliver Schwendener <oliver.schwendener@hotmail.com>
2024-12-02 18:21:52 +0100

    feat(SystemCommands): add option to empty trash (#1275)
    * feat(SystemCommands): add option to empty trash
    
    * Added Linux support for System Command extension
    
    * Added icons for linux

--- src/main/Extensions/SystemCommands/SystemCommand.ts
+++ src/main/Extensions/SystemCommands/SystemCommand.ts
@@ -1,5 +1,7 @@
 import type { SearchResultItem } from "@common/Core";
 
 export interface SystemCommand {
+    getId(): string;
+    invoke: () => Promise<void>;
     toSearchResultItem(): SearchResultItem;
 }
