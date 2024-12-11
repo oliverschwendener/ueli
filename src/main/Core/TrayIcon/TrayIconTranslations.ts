b958c01e8d53188ed76f150a5e2536cc5f799fbe
Oliver Schwendener <oliver.schwendener@proton.me>
2024-12-11 07:55:19 +0100

    feat: added option to enable/disable hotkey from tray icon menu

--- src/main/Core/TrayIcon/TrayIconTranslations.ts
+++ src/main/Core/TrayIcon/TrayIconTranslations.ts
@@ -3,4 +3,6 @@ export type TrayIconTranslations = {
     "trayIcon.contextMenu.quit": string;
     "trayIcon.contextMenu.settings": string;
     "trayIcon.contextMenu.show": string;
+    "trayIcon.contextMenu.hotkey": string;
+    "trayIcon.contextMenu.hotkey.tooltip": string;
 };
