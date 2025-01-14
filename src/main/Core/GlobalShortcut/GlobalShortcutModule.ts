a0d48e6987203c5def6820d4ffff30e0359e4960
Markus Mattes <mmattes87@gmail.com>
2025-01-14 21:55:17 +0100

    fix: map AltGr to Ctrl+Alt for WIndows #589

--- src/main/Core/GlobalShortcut/GlobalShortcutModule.ts
+++ src/main/Core/GlobalShortcut/GlobalShortcutModule.ts
@@ -12,7 +12,13 @@ export class GlobalShortcutModule {
         const hotkeyIsEnabled = () => settingsManager.getValue("general.hotkey.enabled", true);
 
         const registerHotkey = () => {
-            const hotkey = settingsManager.getValue("general.hotkey", "Alt+Space");
+            let hotkey = settingsManager.getValue("general.hotkey", "Alt+Space");
+            const operatingSystem = moduleRegistry.get("OperatingSystem");
+
+            // Replace AltGr with Ctrl+Alt on Windows due to OS mapping
+            if(operatingSystem === "Windows") {
+                hotkey = hotkey.replace('AltGr', 'Ctrl+Alt');
+            }              
 
             if (!isValidHotkey(hotkey)) {
                 logger.error(`Unable to register hotkey. Reason: unexpected hotkey ${hotkey}`);
