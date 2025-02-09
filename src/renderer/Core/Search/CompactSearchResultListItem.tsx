c33a7bf247409b7b7c9bc66e695a616b3e89384a
Oliver Schwendener <oliver.schwendener@proton.me>
2025-02-09 16:56:14 +0100

    Make sure text overflow is rendered with ellipsis

--- src/renderer/Core/Search/CompactSearchResultListItem.tsx
+++ src/renderer/Core/Search/CompactSearchResultListItem.tsx
@@ -1,5 +1,5 @@
 import type { SearchResultItem } from "@common/Core";
-import { Text } from "@fluentui/react-components";
+import { Badge, Text } from "@fluentui/react-components";
 import { useTranslation } from "react-i18next";
 import { SearchResultItemImage } from "./SearchResultItemImage";
 
@@ -16,36 +16,35 @@ export const CompactSearchResultListItem = ({ searchResultItem }: CompactSearchR
                 display: "flex",
                 flexDirection: "row",
                 alignItems: "center",
-                justifyContent: "flex-start",
-                height: 36,
-                padding: 10,
+                justifyContent: "space-between",
+                padding: 8,
                 boxSizing: "border-box",
                 gap: 10,
                 width: "100%",
             }}
         >
-            <SearchResultItemImage image={searchResultItem.image} altText={searchResultItem.name} size={20} />
+            <div style={{ flexShrink: 0 }}>
+                <SearchResultItemImage image={searchResultItem.image} altText={searchResultItem.name} size={20} />
+            </div>
             <Text
-                size={300}
                 style={{
-                    width: "100%",
+                    flexGrow: 1,
+                    whiteSpace: "nowrap",
                     overflow: "hidden",
                     textOverflow: "ellipsis",
-                    whiteSpace: "nowrap",
-                    display: "flex",
-                    alignItems: "center",
-                    gap: 5,
                 }}
             >
                 {searchResultItem.name}
             </Text>
-            <Text size={200} style={{ flexShrink: 0 }}>
-                {searchResultItem.descriptionTranslation
-                    ? t(searchResultItem.descriptionTranslation.key, {
-                          ns: searchResultItem.descriptionTranslation.namespace,
-                      })
-                    : searchResultItem.description}
-            </Text>
+            <div style={{ flexShrink: 0, display: "flex" }}>
+                <Badge color="subtle" size="small">
+                    {searchResultItem.descriptionTranslation
+                        ? t(searchResultItem.descriptionTranslation.key, {
+                              ns: searchResultItem.descriptionTranslation.namespace,
+                          })
+                        : searchResultItem.description}
+                </Badge>
+            </div>
         </div>
     );
 };
