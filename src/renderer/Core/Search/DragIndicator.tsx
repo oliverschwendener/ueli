d51065db1c56c496ed0a8b4b9d5b35abef8a8948
Oliver Schwendener <oliver.schwendener@proton.me>
2025-02-14 22:57:14 +0100

    Added todo

--- src/renderer/Core/Search/DragIndicator.tsx
+++ src/renderer/Core/Search/DragIndicator.tsx
@@ -2,6 +2,8 @@ import { Tooltip } from "@fluentui/react-components";
 import { ProtocolHandlerRegular } from "@fluentui/react-icons";
 
 export const DragIndicator = () => {
+    // TODO: add translations
+
     return (
         <Tooltip content="Can be dragged" relationship="label">
             <ProtocolHandlerRegular />
