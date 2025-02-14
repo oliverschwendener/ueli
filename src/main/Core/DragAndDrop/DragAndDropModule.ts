d0b18952ab06b2f474565cb9d6df93dca3b8bce1
Oliver Schwendener <oliver.schwendener@proton.me>
2025-02-14 22:58:00 +0100

    Simplified code

--- src/main/Core/DragAndDrop/DragAndDropModule.ts
+++ src/main/Core/DragAndDrop/DragAndDropModule.ts
@@ -5,12 +5,11 @@ import { app } from "electron";
 export class DragAndDropModule {
     public static bootstrap(moduleRegistry: UeliModuleRegistry) {
         const ipcMain = moduleRegistry.get("IpcMain");
-        ipcMain.on("dragStarted", async ({ sender }, { filePath }: DragAndDrop) => {
-            const icon = await app.getFileIcon(filePath);
 
+        ipcMain.on("dragStarted", async ({ sender }, { filePath }: DragAndDrop) => {
             sender.startDrag({
                 file: filePath,
-                icon,
+                icon: await app.getFileIcon(filePath),
             });
         });
     }
