4a36abe8165ac34d1e88dc28cacaa9b90a899d07
Oliver Schwendener <oliver.schwendener@hotmail.com>
2025-03-01 21:49:10 +0100

    fix: remove not working drag and drop for linux applications (#1383)

--- src/main/Extensions/ApplicationSearch/Linux/LinuxApplication.ts
+++ src/main/Extensions/ApplicationSearch/Linux/LinuxApplication.ts
@@ -19,7 +19,6 @@ export class LinuxApplication implements Application {
                 namespace: "extension[ApplicationSearch]",
             },
             details: this.filePath,
-            dragAndDrop: { filePath: this.filePath },
             id: this.getId(),
             name: this.name,
             image: this.image,
