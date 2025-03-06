db9f4d7788d1dbf5fef3125a6095346f3bc7b0ca
Oliver Schwendener <oliver.schwendener@proton.me>
2025-03-06 08:18:20 +0100

    Add todo

--- src/main/Core/SettingsFile/SettingsFilePathResolver.ts
+++ src/main/Core/SettingsFile/SettingsFilePathResolver.ts
@@ -5,6 +5,8 @@ export class SettingsFilePathResolver {
     public constructor(private readonly app: App) {}
 
     public async resolve(): Promise<string> {
+        // TODO add option to get custom file path from env variable or command line argument
+
         return this.getDefaultSettingsFilePath();
     }
 
