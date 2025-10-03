fdec9e5bb906b7b1679132401c1e77a4e355fca9
cou <courange.c@gmail.com>
2025-10-03 15:28:19 +0900

    refactor(NotificationModule): update app icon file path resolution

--- src/main/Core/Notification/NotificationModule.ts
+++ src/main/Core/Notification/NotificationModule.ts
@@ -1,12 +1,18 @@
+import { AppIconFilePathResolver } from "@Core/BrowserWindow/AppIconFilePathResolver";
 import type { UeliModuleRegistry } from "@Core/ModuleRegistry";
 import { ElectronNotificationService } from "./NotificationService";
 
 export class NotificationModule {
     public static bootstrap(moduleRegistry: UeliModuleRegistry) {
         const assetPathResolver = moduleRegistry.get("AssetPathResolver");
+        const appIconFilePathResolver = new AppIconFilePathResolver(
+            moduleRegistry.get("NativeTheme"),
+            assetPathResolver,
+            moduleRegistry.get("OperatingSystem"),
+        );
         moduleRegistry.register(
             "NotificationService",
-            new ElectronNotificationService(assetPathResolver.getModuleAssetPath("Notification", "icon.png")),
+            new ElectronNotificationService(appIconFilePathResolver.getAppIconFilePath()),
         );
     }
 }
