3e18b7268185ade234cc34e8b4e5a7baffe9c113
cou <courange.c@gmail.com>
2025-10-28 23:37:41 +0900

    fix: use appIconFilePathResolver in NotificationModule

--- src/main/Core/Notification/Notification.ts
+++ src/main/Core/Notification/Notification.ts
@@ -1,15 +1,16 @@
+import { type AppIconFilePathResolver } from "@Core/AppIconFilePathResolver/Contract";
 import { Notification } from "electron";
 import { type Notification as NotificationInterface } from "./Contract";
 
 export class ElectronNotification implements NotificationInterface {
-    public constructor(private readonly icon: string) {}
+    public constructor(private readonly appIconFilePathResolver: AppIconFilePathResolver) {}
 
     public show({ title, body }: { title: string; body: string }): void {
         if (!Notification.isSupported()) {
             return;
         }
 
-        const notification = new Notification({ title, body, icon: this.icon });
+        const notification = new Notification({ title, body, icon: this.appIconFilePathResolver.getAppIconFilePath() });
         notification.show();
     }
 }
--- src/main/Core/Notification/NotificationModule.ts
+++ src/main/Core/Notification/NotificationModule.ts
@@ -1,10 +1,9 @@
-import type { AppIconFilePathResolver } from "@Core/AppIconFilePathResolver";
 import type { UeliModuleRegistry } from "@Core/ModuleRegistry";
 import { ElectronNotification } from "./Notification";
 
 export class NotificationModule {
     public static bootstrap(moduleRegistry: UeliModuleRegistry) {
-        const appIconFilePathResolver = moduleRegistry.get("AppIconFilePathResolver") as AppIconFilePathResolver;
-        moduleRegistry.register("Notification", new ElectronNotification(appIconFilePathResolver.getAppIconFilePath()));
+        const appIconFilePathResolver = moduleRegistry.get("AppIconFilePathResolver");
+        moduleRegistry.register("Notification", new ElectronNotification(appIconFilePathResolver));
     }
 }
